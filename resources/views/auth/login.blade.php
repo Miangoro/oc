ba86ac33f5fe5de518839d310f44b23fd38dcf00
Miangoro <miguel.gmz.itshmail.com>
2024-06-24 18:08:22 -0600

    Varios cambios en el login 2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@ $customizerHidden = 'customizer-hide';
 
 @extends('layouts/blankLayout')
 
-@section('title', 'Login')
+@section('title', 'Iniciar sesión')
 
 @section('page-style')
 {{-- Page Css files --}}
@@ -14,26 +14,41 @@ $customizerHidden = 'customizer-hide';
 @endsection
 
 @section('content')
+
+<style>
+  video {
+          position: fixed;
+          right: 0;
+          bottom: 0;
+          min-width: 105%;
+          min-height: 100%;
+          transform: translateX(calc((100% - 100vw) / 2));
+          z-index: -2;
+        }
+</style>
 <div class="authentication-wrapper authentication-cover">
   <!-- Logo -->
   <a href="{{url('/')}}" class="auth-cover-brand d-flex align-items-center gap-2">
-    <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-    <span class="app-brand-text demo text-heading fw-semibold">{{config('variables.templateName')}}</span>
+    <span class="app-brand-logo demo"><img height="150px" src="{{ asset('assets/img/branding/logo.png')}}" alt=""></span>
+    <!--<span class="app-brand-text demo text-heading fw-semibold">{{config('variables.templateName')}}</span>-->
   </a>
   <!-- /Logo -->
   <div class="authentication-inner row m-0">
     <!-- /Left Section -->
     <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-12 pb-2">
-      <img src="{{asset('assets/img/illustrations/auth-login-illustration-'.$configData['style'].'.png') }}" class="auth-cover-illustration w-100" alt="auth-illustration" data-app-light-img="illustrations/auth-login-illustration-light.png" data-app-dark-img="illustrations/auth-login-illustration-dark.png" />
-      <img src="{{asset('assets/img/illustrations/auth-cover-login-mask-'.$configData['style'].'.png') }}" class="authentication-image" alt="mask" data-app-light-img="illustrations/auth-cover-login-mask-light.png" data-app-dark-img="illustrations/auth-cover-login-mask-dark.png" />
+       <video  autoplay muted loop>
+        <source src="{{ asset('video/fondo.mp4')}}" type="video/mp4">
+         Tu navegador no soporta el formato de video
+      </video>
     </div>
     <!-- /Left Section -->
 
     <!-- Login -->
     <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg position-relative py-sm-12 px-12 py-6">
       <div class="w-px-400 mx-auto pt-5 pt-lg-0">
-        <h4 class="mb-1">Welcome to {{config('variables.templateName')}}! 👋</h4>
-        <p class="mb-5">Please sign-in to your account and start the adventure</p>
+        <img height="150px" src="{{ asset('assets/img/branding/logo.png')}}" alt="">
+        <h4 class="mb-1">Bienvenido a {{config('variables.templateName')}} </h4>
+        <p class="mb-5">Por favor, inicie sesión</p>
 
         @if (session('status'))
           <div class="alert alert-success mb-3" role="alert">
@@ -46,7 +61,7 @@ $customizerHidden = 'customizer-hide';
           @csrf
           <div class="form-floating form-floating-outline mb-5">
             <input type="text" class="form-control @error('email') is-invalid @enderror" id="login-email" name="email" placeholder="john@example.com" autofocus value="{{ old('email') }}">
-            <label for="login-email">Email</label>
+            <label for="login-email">Correo</label>
             @error('email')
               <span class="invalid-feedback" role="alert">
                 <span class="fw-medium">{{ $message }}</span>
@@ -61,7 +76,7 @@ $customizerHidden = 'customizer-hide';
                     class="form-control @error('password') is-invalid @enderror" name="password"
                     placeholder="&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;&#xb7;"
                     aria-describedby="password" />
-                  <label for="login-password">Password</label>
+                  <label for="login-password">Contraseña</label>
                 </div>
                 <span class="input-group-text cursor-pointer"><i class="ri-eye-off-line"></i></span>
               </div>
@@ -76,17 +91,17 @@ $customizerHidden = 'customizer-hide';
             <div class="form-check mt-2">
               <input class="form-check-input" type="checkbox" id="remember-me">
               <label class="form-check-label" for="remember-me">
-                Remember Me
+                Recuerdame
               </label>
             </div>
             @if (Route::has('password.request'))
               <a href="{{ route('password.request') }}" class="float-end mb-1 mt-2">
-                <span>Forgot Password?</span>
+                <span>¿Olvidó su contraseña?</span>
               </a>
             @endif
           </div>
           <button class="btn btn-primary d-grid w-100">
-            Sign in
+           Iniciar sesión
           </button>
         </form>
 
