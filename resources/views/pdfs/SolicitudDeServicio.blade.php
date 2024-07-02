6ea1e86de2704f5a18d7e1de27fa425706fa870b
Yexqls <yucli.emmanuel.666@gmail.com>
2024-07-02 10:52:10 -0600

    servicios

--- resources/views/pdfs/SolicitudDeServicio.blade.php
+++ resources/views/pdfs/SolicitudDeServicio.blade.php
@@ -30,6 +30,7 @@
             border: 1px solid black;
             padding: 8px;
             text-align: center;
+            font-weight: bold;
         }
 
         th {
@@ -59,6 +60,9 @@
         .th-color {
             background-color: #d8d8d8;
         }
+        .sin-negrita {
+        font-weight: normal;
+    }
     </style>
 </head>
 
@@ -73,13 +77,13 @@
                     </center>
                 </div>
             </td>
-            <td rowspan="2" colspan="5">SOLICITUD DE SERVICIOS</td>
-            <td colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
+            <td rowspan="2" colspan="5" style="font-size: 12px">SOLICITUD DE SERVICIOS</td>
+            <td class="sin-negrita" colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
                 Edición 10 Entrada en vigor:
                 20/06/2024</td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td class="sin-negrita" colspan="3">
                 ORGANISMO DE CERTIFICACION No. de <br> acreditación 144/18 ante la ema A.C
             </td>
         <tr>
@@ -267,8 +271,13 @@
             <td colspan="4">&nbsp;</td>
         </tr>
         <tr>
-            <td class="td-margins" colspan="9" style="padding: .05px">&nbsp;</td>
+            <td class="td-no-margins" colspan="9">Este documento es propiedad del Centro de Innovación y Desarroll
+                Agroalimentario de Michoacán A.C.</td>
         </tr>
+        <tr>
+            <td class="td-no-margins">&nbsp;</td>
+        </tr>
+
         <tr>
             <td class="td-margins letra_td">Emisión de certificado venta nacional</td>
             <td>&nbsp;</td>
@@ -277,13 +286,7 @@
             <td colspan="2" class="td-no-margins letra_td">Fecha de emisión <br>propuesta:</td>
             <td colspan="4">&nbsp;</td>
         </tr>
-        <tr>
-            <td class="td-no-margins" colspan="9">Este documento es propiedad del Centro de Innovación y Desarroll
-                Agroalimentario de Michoacán A.C.</td>
-        </tr>
-        <tr>
-            <td class="td-no-margins">&nbsp;</td>
-        </tr>
+
         <tr>
             <td rowspan="2" class="td-margins letra_td">Dictaminación de instalaciones:</td>
             <td rowspan="2">&nbsp;</td>
@@ -364,8 +367,16 @@
         </tr>
 
     </table>
-    <br>
     <table>
+        
+        <tr>
+            <td class="td-no-margins sin-negrita" colspan="9" style="padding-top: 550px">Este documento es propiedad del Centro de Innovación y Desarroll
+                Agroalimentario de Michoacán A.C.</td>
+        </tr>
+        <tr>
+            <td class="td-no-margins">&nbsp;</td>
+        </tr>
+
         <tr>
             <td rowspan="3">
                 <div>
@@ -375,13 +386,13 @@
                     </center>
                 </div>
             </td>
-            <td rowspan="2" colspan="5">SOLICITUD DE SERVICIOS</td>
-            <td colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
+            <td rowspan="2" colspan="5" style="font-size: 12px">SOLICITUD DE SERVICIOS</td>
+            <td class="sin-negrita" colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
                 Edición 10 Entrada en vigor:
                 20/06/2024</td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td class="sin-negrita" colspan="3">
                 ORGANISMO DE CERTIFICACION No. de <br> acreditación 144/18 ante la ema A.C
             </td>
         <tr>
@@ -395,8 +406,14 @@
             <td colspan="6">&nbsp;</td>
         </tr>
     </table>
-    <br>
     <table>
+        <tr>
+            <td class="td-no-margins sin-negrita" colspan="9"  style="padding-top: 790px">Este documento es propiedad del Centro de Innovación y Desarroll
+                Agroalimentario de Michoacán A.C.</td>
+        </tr>
+        <tr>
+            <td class="td-no-margins">&nbsp;</td>
+        </tr>
         <tr>
             <td rowspan="3">
                 <div>
@@ -406,13 +423,13 @@
                     </center>
                 </div>
             </td>
-            <td rowspan="2" colspan="5">SOLICITUD DE SERVICIOS</td>
-            <td colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
+            <td rowspan="2" colspan="5" style="font-size: 12px">SOLICITUD DE SERVICIOS</td>
+            <td class="sin-negrita" colspan="3" style="text-align: right">Solicitud de servicios NOM-070-SCFI-2016 F7.1-01-32 <br>
                 Edición 10 Entrada en vigor:
                 20/06/2024</td>
         </tr>
         <tr>
-            <td colspan="3">
+            <td class="sin-negrita" colspan="3">
                 ORGANISMO DE CERTIFICACION No. de <br> acreditación 144/18 ante la ema A.C
             </td>
         <tr>
@@ -436,7 +453,7 @@
         <tr>
             <td>&nbsp;</td>
             <td>&nbsp;</td>
-            <td colspan="7">Copia del análisis de laboratorio de cada uno de los lotes en cumplimiento con el
+            <td class="sin-negrita" colspan="7">Copia del análisis de laboratorio de cada uno de los lotes en cumplimiento con el
                 apartado 4.3 de la NOM-070-SCFI-2016. En caso de <br>
                 producto cuente con ajuste de grado alcohólico, reposado o añejo adjuntar copia de los analisis de
                 laboratorio posteriores al proceso <br>
@@ -463,7 +480,7 @@
         </tr>
 
         <tr>
-            <td colspan="4">Se cuenta con todos los medios para realizar todas las actividades de evaluación para la <br>
+            <td class="sin-negrita" colspan="4">Se cuenta con todos los medios para realizar todas las actividades de evaluación para la <br>
                 Certificación:</td>
             <td>&nbsp;</td>
             <td>&nbsp;</td>
@@ -471,22 +488,29 @@
                 viabilidad del servicio:</td>
         </tr>
         <tr>
-            <td colspan="4">El organismo de Certificación tiene la competencia para realizar la Certificación</td>
+            <td class="sin-negrita" colspan="4">El organismo de Certificación tiene la competencia para realizar la Certificación</td>
             <td>&nbsp;</td>
             <td>&nbsp;</td>
         </tr>
         <tr>
-            <td colspan="4">El organismo de Certificación tiene la capacidad para llevar a cabo las actividades de <br>
+            <td class="sin-negrita" colspan="4">El organismo de Certificación tiene la capacidad para llevar a cabo las actividades de <br>
                 certificación</td>
                 <td>&nbsp;</td>
                 <td>&nbsp;</td>
                 <td rowspan="2" colspan="3">Nombre y firma</td>
         </tr>
-        <td colspan="2">Comentarios:</td>
+        <td class="sin-negrita" colspan="2">Comentarios:</td>
             <td colspan="4">&nbsp;</td>
     </tr>
 
     </table>
+    <table>        <tr>
+        <td class="td-no-margins sin-negrita" colspan="9"  style="padding-top: 400px">Este documento es propiedad del Centro de Innovación y Desarroll
+            Agroalimentario de Michoacán A.C.</td>
+    </tr>
+    <tr>
+        <td class="td-no-margins">&nbsp;</td>
+    </tr></table>
 
 </body>
 
