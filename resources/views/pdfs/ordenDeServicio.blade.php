306283e0520bcdd76c4587c07d12b195355776a5
Yexqls <yucli.emmanuel.666@gmail.com>
2024-08-19 13:42:29 -0600

    fot

--- resources/views/pdfs/ordenDeServicio.blade.php
+++ resources/views/pdfs/ordenDeServicio.blade.php
@@ -33,7 +33,7 @@
         }
 
         .footer {
-            transform: translate(0px, 220px);
+            transform: translate(0px, 180px);
             /* Mueve el elemento 50px en X y 50px en Y */
             text-align: center;
             font-size: 11px;
