99699e0cf84abb616ce6734cf48567bf07d555d5
Yexqls <yucli.emmanuel.666@gmail.com>
2024-08-22 16:51:10 -0600

    pdffixmargenes

--- resources/views/pdfs/solicitudDeHologramas.blade.php
+++ resources/views/pdfs/solicitudDeHologramas.blade.php
@@ -13,8 +13,8 @@
         @page {
             margin-left: 43px;
             margin-right: 43px;
-            margin-top: 30px;
-            margin-bottom: 10px;
+            margin-top: 43px;
+            margin-bottom: 43px;
 
         }
 
@@ -110,14 +110,14 @@
         .leftLetter {
             text-align: left;
             vertical-align: top;
-            padding-bottom: 10px;
+            padding-bottom: 8px;
             padding-top: 0;
         }
 
         .rightLetter {
             text-align: right;
             vertical-align: top;
-            padding-bottom: 10px;
+            padding-bottom: 8px;
             padding-top: 0;
         }
 
@@ -131,11 +131,12 @@
 
         .letra-fondo{
             color: white;
-            
             font-size: 19px;
             background-color: #006fc0;
-            padding-bottom: 21px;
+            padding-bottom: 18px;
             text-align: center;
+            padding-top: 0;
+
         }
 
         .letra-up{
@@ -154,16 +155,18 @@
             <img src="{{ public_path('img_pdf/logo_cidam_texto.png') }}" alt="Logo CIDAM"
             style="max-width: 125px; padding: 0;">
         </td>
-          <td rowspan="3" class="title"> Solicitud de hologramas</td>
-          <td colspan="2" class="leftLetter" style="width: 180px"> <b>Identificación:</b> P7.1-01-05</td>
+          <td rowspan="3" class="title negrita"> Solicitud de hologramas</td>
+          <td colspan="2" class="leftLetter " style="width: 180px"> <span class="negrita" style="margin: 0;">Identificación:</span> P7.1-01-05</td>
         </tr>
         <tr>
-          <td colspan="2" class="leftLetter"> <b>Edición:</b> 3</td>
+          <td colspan="2" class="leftLetter "> <span class="negrita" style="margin: 0;">Edición:</span> 3</td>
         </tr>
         <tr>
-          <td colspan="2" class="leftLetter" style="padding-bottom: 15px"> <b>Inicio de Vigencia:</b> <br>
-            15/08/2024</td>
-        </tr>
+            <td colspan="2" class="leftLetter">
+              <span class="negrita" style="margin: 0;">Inicio de Vigencia:</span> <br>15/08/2024
+            </td>
+          </tr>
+          
 
       </table>
 
@@ -172,7 +175,7 @@
     <table>
         <tr>
             <td colspan="2" class="td-no-border" style="width: 380px">&nbsp;</td>
-            <td class="no-padding"> <b>Folio de solicitud:</b> </td>
+            <td class="no-padding"> <p class="negrita" style="margin: 0"> Folio de solicitud:</p> </td>
             <td class="no-padding"> INV-423/2024</td>
         </tr>
     </table>
@@ -187,15 +190,15 @@
             <td class="letra-up"> MAYTE ERÉNDIRA ENRÍQUEZ ROJAS</td>
         </tr>
         <tr>
-            <td class="rightLetter"><b>Puesto:</b> </td>
+            <td class="rightLetter negrita">Puesto: </td>
             <td class="letra-up">representante legal</td>
         </tr>
         <tr>
-            <td class="rightLetter"><b>Email:</b> </td>
+            <td class="rightLetter negrita">Email: </td>
             <td class="letra-up">mezcaldesanluis@gmail.com</td>
         </tr>
         <tr>
-            <td style="border-bottom: 0;" colspan="2" class="letra-fondo"> Dirección de envío</td>
+            <td style="border-bottom: 0;" colspan="2" class="letra-fondo negrita"> Dirección de envío</td>
         </tr>
     </table>
 
@@ -205,49 +208,49 @@
     </td>
 </tr>
 <tr>
-    <td colspan="4" class="letra-fondo"> INFORMACIÓN DE QUIEN RECIBE EL PAQUETE DE HOLOGRAMAS
+    <td colspan="4" class="letra-fondo negrita"> INFORMACIÓN DE QUIEN RECIBE EL PAQUETE DE HOLOGRAMAS
     </td>
 </tr>
 <tr>
-    <td class="rightLetter" style="width: 110px"><b>Nombre Completo: </b> </td>
+    <td class="rightLetter negrita" style="width: 120px">Nombre Completo: </td>
     <td colspan="3" class="letra-up">Nazareth Camacho</td>
 </tr>
 <tr>
-    <td class="rightLetter"><b>Email./Cel./Tel.:</b> </td>
+    <td class="rightLetter negrita">Email./Cel./Tel.: </td>
     <td colspan="3" class="letra-up"> mezcaldesanluis@gmail.com | 9512432784</td>
 </tr>
 
 <tr>
-    <td class="rightLetter"><b>Fecha de envío:</b></td>
+    <td class="rightLetter negrita">Fecha de envío:</td>
     <td class="letra-up">2024 de Agosto del 12</td>
-    <td class="rightLetter"> <b>Fecha de recibido:</b></td>
+    <td class="rightLetter negrita" style="width: 110px"> Fecha de recibido:</td>
     <td></td>
 </tr>
 <tr>
-    <td class="rightLetter"><b>Folio inicial:</b></td>
+    <td class="rightLetter negrita" >Folio inicial:</td>
     <td class="letra-up">NOM-070-054CB000000</td>
-    <td class="rightLetter"><b>Folio final:</b></td>
+    <td class="rightLetter negrita">Folio final:</td>
     <td class="letra-up">NOM-070-054CB000000</td>
 </tr>
 <tr>
-    <td class="rightLetter" ><b>Total de hologramas <br>
-        enviados:</b></td>
+    <td class="rightLetter negrita" >Total de hologramas <br>
+        enviados:</td>
     <td colspan="3" class="letra-up">5,000 Hologramas</td>
 </tr>
 <tr >
-    <td class="rightLetter" style="height: 70px"><b>Comentarios</b></td>
+    <td class="rightLetter negrita" style="height: 60px">Comentarios</td>
     <td colspan="3"></td>
 </tr>
 <tr>
     <td colspan="4">NOTA: Se solicita reenviar vía electrónica este acuse firmado para confirmar la llegada de hologramas. <div style="height: 50px"></div>
         _____________________________________________ <br>
-        <b>Nazareth Camacho </b>
+        <p class="negrita">Nazareth Camacho </p>
         <div style="height: 10px"></div>
     </td>
 </tr>
     </table>
     <div style="margin-bottom: 15px">
-        <p style="font-size: 12px; margin-top: 120px; text-align: center">Este documento es propiedad del Centro de Innovación y Desarrollo Agroalimentario de Michoacán A.C. y no puede ser distribuido externamente sin la
+        <p style="font-size: 12px; margin-top: 50px; text-align: center">Este documento es propiedad del Centro de Innovación y Desarrollo Agroalimentario de Michoacán A.C. y no puede ser distribuido externamente sin la
             autorización escrita del Director Ejecutivo.</p>
     </div>
 
