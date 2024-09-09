a62d5dffc8bbe94b0d883c12292705f69772c04c
Yexqls <yucli.emmanuel.666@gmail.com>
2024-09-09 10:41:05 -0600

    pdf

---
+++ resources/views/pdfs/CertificadoDeConformidadNOM-199.blade.php
@@ -0,0 +1,395 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Certificado de conformidad NOM-199-SCFI-2017</title>
+    <style>
+        body {
+            font-weight: 12px;
+        }
+
+        @page {
+            margin-left: 80px;
+            margin-right: 80px;
+            margin-top: 40px;
+            margin-bottom: 10px;
+
+        }
+
+        @font-face {
+            font-family: 'Century Gothic';
+            src: url('fonts/CenturyGothic.ttf') format('truetype');
+
+        }
+
+        @font-face {
+            font-family: 'Century Gothic Negrita';
+            src: url('fonts/GOTHICB.TTF') format('truetype');
+        }
+
+
+        body {
+            font-family: 'Century Gothic', sans-serif;
+            font-size: 10px;
+
+        }
+
+        .negrita {
+            color: #cb8f2b;
+            font-size: 14px;
+            font-family: 'Century Gothic Negrita';
+        }
+
+
+        .header {
+            text-align: right;
+            font-size: 12px;
+            margin-right: -30px;
+
+        }
+
+        .title {
+            text-align: center;
+            font-size: 17px;
+        }
+
+        .footer {
+            position: absolute;
+            transform: translate(0px, 180px);
+            /* Mueve el elemento 50px en X y 50px en Y */
+            text-align: center;
+            font-size: 11px;
+        }
+
+        /*Tablas*/
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        th,
+        td {
+            border: 2px solid #cb8f2b;
+            padding: 4px;
+            font-size: 11px;
+            text-align: center;
+            font-family: 'Century Gothic';
+
+
+
+        }
+
+        th {
+            background-color: black;
+            color: white;
+            text-align: center;
+
+        }
+
+        .td-border {
+            border-bottom: 3px solid #cb8f2b;
+            border-top: none;
+            border-right: 3px solid #cb8f2b;
+            border-left: 3px solid #cb8f2b;
+
+        }
+
+        .td-no-border {
+            border: none;
+        }
+
+        .td-barra {
+            border-bottom: none;
+            border-top: none;
+            border-right: none;
+            border-left: 1px solid black;
+        }
+
+        .letra_td {
+            text-align: right;
+        }
+
+        .th-color {
+            background-color: #d8d8d8;
+        }
+
+        .leftLetter {
+            text-align: left;
+
+        }
+
+        .rightLetter {
+            text-align: right;
+            vertical-align: top;
+            padding-bottom: 8px;
+            padding-top: 0;
+        }
+
+        .no-margin-top {
+            margin-top: 0;
+        }
+
+        .no-padding {
+            padding: 0;
+        }
+
+        .no-padding-up-down {
+            padding-top: 0;
+            padding-bottom: 0;
+        }
+
+
+        .no-padding-r-l {
+            padding-right: 0;
+            padding-left: 0;
+        }
+
+        .letra-fondo {
+            color: white;
+            font-size: 12px;
+            background-color: #028457;
+            text-align: center;
+            font-family: 'Century Gothic Negrita';
+
+
+        }
+
+        .letra-up {
+            vertical-align: top;
+            padding-bottom: 10px;
+            padding-top: 0;
+
+        }
+
+        .header {
+            padding: 10px;
+            text-align: center;
+            font-size: 10px;
+        }
+
+        .header img {
+            max-width: 300px;
+            padding: 0;
+            margin-top: -30px;
+
+        }
+
+        .img-background {
+            position: absolute;
+            top: 567px;
+            left: -70px;
+            width: 820px;
+            height: 450px;
+            z-index: -1;
+            background-image: url('{{ public_path('img_pdf/membretado_certificado_199.png') }}');
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-position: center;
+        }
+
+        .page-break {
+            page-break-before: always;
+        }
+
+        /* Estilo para la imagen flotante a la derecha */
+        /* Estilo para el texto de fondo */
+        .background-img {
+            position: absolute;
+            top: 930px;
+            left: 510px;
+            z-index: -1;
+            width: 180px;
+            height: 75px;
+
+        }
+
+
+        .letra1 {
+            color: #0c1344;
+            font-size: 15px;
+            font-family: 'Century Gothic Negrita';
+
+        }
+
+        .letra2 {
+            color: #cb8f2b;
+            font-size: 11px;
+
+        }
+
+        .letra3 {
+            color: #cb8f2b;
+            font-size: 32px;
+            font-family: 'Century Gothic Negrita';
+
+        }
+    </style>
+</head>
+
+<body>
+    <div class="img-background"></div>
+
+    <div class="header">
+        <center> <img src="{{ public_path('img_pdf/logo_oc_3d.png') }}" alt="Logo CIDAM"></center>
+        <div> <span class="letra1">Centro de Innovación y Desarrollo Agroalimentario de Michoacán, </span><br>
+            <span class="letra2"> Acreditado como Organismo de Certificación de producto con número de acreditación
+                144/18 <br>
+                ante la Entidad Mexicana de Acreditación, A.C.</span>
+        </div>
+        <br>
+        <div style="color:  #0c1344; font-size: 13px"><span>Otorga el siguiente:</span> <br>
+            <span style="font-size: 24px">CERTIFICADO DE CONFORMIDAD A:</span><br>
+            <span class="letra3">XXXXXXXXX</span>
+        </div>
+        <div>
+            <span class="letra1"> RFC: </span><br>
+            <span style="color: #0c1344; font-size: 14px"> Domicilio</span>
+        </div>
+    </div>
+
+    <body>
+        <div style="color: #0c1344; text-align: left; font-size: 13px">
+            Por haber demostrado que cuenta con la infraestructura, equipamiento y la competencia necesaria <br>
+            para realizar el proceso de producción de las bebidas:
+
+        </div>
+        <br>
+        <table>
+            <tr>
+                <td class="negrita">Familia</td>
+                <td class="negrita">Denominación <br>
+                    Genérica <br>
+                    (Base Alcohólica)</td>
+                <td class="negrita">Sabor</td>
+                <td class="negrita">Marca</td>
+                <td class="negrita">Presentación</td>
+            </tr>
+            <tr>
+                <td style="height: 100px"></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+<div>
+    Este certificado de conformidad de Bebidas Alcohólicas se expide de acuerdo a la Norma Oficial <br>
+Mexicana NOM-199-SCFI-2017, Bebidas Alcohólicas-Denominación, Especificaciones <br>
+Fisicoquímicas, Información Comercial y Métodos de Prueba. 
+</div>
+        <img src="{{ public_path('img_pdf/logo_oc_3d.png') }}" alt="Logo CIDAM" class="background-img">
+        <footer style="text-align: center; font-size: 7px; margin-top: 175px; color: #71777c; margin-right: 50px">
+            Este documento es propiedad del Centro de Innovación y Desarrollo Agroalimentario de Michoacán A.C. y no
+            puede <br>
+            ser distribuido externamente sin la autorización escrita del Director Ejecutivo
+
+        </footer>
+        <div class="page-break"></div>
+        <div class="img-background"></div>
+
+        <div class="header">
+            <img src="{{ public_path('img_pdf/logo_cidam_texto.png') }}" alt="Logo CIDAM">
+            <div style="padding-right: 20px">Plan de auditoría de esquema de certificación NOM-070-SCFI-2016
+                F7.1-01-13<br>Edición 2 Entrada en Vigor: 08/03/2023
+                <br>_______________________________________________________________________________________
+            </div>
+        </div>
+        <div style="height: 40px"></div>
+        <table>
+            <tr>
+                <td class="letra-fondo" style="font-size: 14px" colspan="3">5. DATOS DEL GRUPO EVALUADOR
+                </td>
+            </tr>
+            <tr>
+                <td class="letra-fondo">Designación:</td>
+                <td class="letra-fondo">Nombre: </td>
+                <td class="letra-fondo">Teléfono y correo electrónico:</td>
+            </tr>
+            <tr>
+                <td>Inspector</td>
+                <td>Erik Antonio Mejía Vaca</td>
+                <td>4521971634, emejia@erpcidam.com</td>
+            </tr>
+            <tr>
+                <td>Auditor</td>
+                <td></td>
+                <td></td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td class="letra-fondo" style="font-size: 14px" colspan="5">6. DESCRIPCIÓN DE ACTIVIDADES DE
+                    AUDITORÍA
+                </td>
+            </tr>
+            <tr>
+                <td class="letra-fondo no-padding">Fecha:</td>
+                <td class="letra-fondo no-padding">Inspector/ <br>
+                    Auditor:</td>
+                <td class="letra-fondo" style="width: 370px">Actividad:</td>
+                <td class="letra-fondo no-padding">Horario:</td>
+                <td class="letra-fondo no-padding">Aplica <br>
+                    (Auditados)</td>
+            </tr>
+            <tr>
+                <td class="no-padding">2024-09-02</td>
+                <td class="no-padding">Erik Antonio <br>
+                    Mejía Vaca</td>
+                <td class="no-padding">Reunión de apertura</td>
+                <td class="no-padding"></td>
+                <td class="no-padding">Aplica</td>
+            </tr>
+        </table>
+        <br>
+        <table>
+            <tr>
+                <td class="letra-fondo" style="font-size: 14px" colspan="4">6. DESCRIPCIÓN DE ACTIVIDADES DE
+                    AUDITORÍA
+                </td>
+            </tr>
+            <tr>
+                <td class="letra-fondo no-padding-up-down">Nombre del Inspector:</td>
+                <td class="no-padding-up-down" colspan="3">Erik Antonio Mejía Vaca
+                </td>
+            </tr>
+            <tr>
+                <td class="letra-fondo no-padding-r-l">Acepta o rechaza el plan de <br>
+                    auditoría:</td>
+                <td style="width: 140px">Aprobado</td>
+                <td class="letra-fondo no-padding-r-l">Nombre del cliente o persona <br>
+                    autorizada que acepta o rechaza <br>
+                    el plan
+                </td>
+                <td style="width: 140px">PRISCILIANO <br>
+                    MARTÍNEZ</td>
+            </tr>
+            <tr>
+                <td class="letra-fondo">Políticas:</td>
+                <td class="leftLetter" style="font-size: 8px" colspan="3">1. Aceptar o rechazar el presente plan
+                    de auditoria antes de 48 horas, de lo contrario se considera <br>
+                    aceptado.<br>
+                    2. Comunicarse previamente, mínimo 3 días previos a la auditoria, con el auditor asignado por el<br>
+                    Organismo<br>
+                    Certificador con la finalidad de coordinar la actividad en sitio.<br>
+                    3. En caso de conflicto de interés, se debe notificar al Organismo de Certificación previamente.<br>
+                    4. Para realizarel servicio de certificación se contratará una Unidad de Verificación y Laboratorios
+                    de<br>
+                    pruebas.</td>
+            </tr>
+        </table>
+        <img src="{{ public_path('img_pdf/logo_oc_3d.png') }}" alt="Logo CIDAM" class="background-img"
+            style="position: absolute">
+        <footer style="text-align: center; font-size: 7px; margin-top: 315px; color: #71777c; margin-right: 50px">
+            Este documento es propiedad del Centro de Innovación y Desarrollo Agroalimentario de Michoacán A.C. y no
+            puede <br>
+            ser distribuido externamente sin la autorización escrita del Director Ejecutivo
+
+        </footer>
+    </body>
+
+
+</html>
