d6a64b5a3461ef8156c42b1d3ca25b442fc10e39
Carlos Zarco <cardrooiid@gmail.com>
2024-09-12 10:31:10 -0600

    PDF Esquema-certificacion

--- resources/views/pdfs/Plan_auditoría_esquema.blade.php
+++ resources/views/pdfs/Plan_auditoría_esquema.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Plan de auditoría de esquema de certificación</title>
 
     <style>
         body {
