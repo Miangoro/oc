896d84be34674711938cb20d7af4448f057454e7
Yexqls <yucli.emmanuel.666@gmail.com>
2024-10-25 16:16:26 -0600

    fixlotesicono/featurure-verguias

--- resources/js/lotes_envasado.js
+++ resources/js/lotes_envasado.js
@@ -209,34 +209,27 @@ function initializeSelect2($elements) {
             searchable: false,
             orderable: false,
             render: function (data, type, full, meta) {
-              return (
-                '<div class="d-flex align-items-center gap-50">' +
-                  // Botón de Opciones con dropend para asegurar que se muestre correctamente
-                  '<div class="btn-group dropend">' +
-                    '<button class="btn btn-sm btn-info dropdown-toggle hide-arrow" data-bs-toggle="dropdown" aria-expanded="false">' +
-                      '<i class="ri-settings-5-fill"></i>&nbsp;Opciones' +
-                    '</button>' +
-                    // Menú desplegable
-                    '<ul class="dropdown-menu dropdown-menu-end m-0">' +
-                      // Opción Editar lotes envasado
-                      '<li><a data-id="' + full['id_lote_envasado'] + '" data-bs-toggle="modal" data-bs-target="#editLoteEnvasado" class="dropdown-item edit-record">' +
-                      '<i class="ri-edit-box-line ri-20px text-info"></i> Editar lotes envasado</a></li>' +
-          
-                      // Opción Reclasificación SKU
-                      '<li><a data-id="' + full['id_lote_envasado'] + '" data-bs-toggle="modal" data-bs-target="#reclasificacion" class="dropdown-item edit-reclasificacion">' +
-                      '<i class="ri-file-settings-line ri-20px text-success"></i> Reclasificación SKU</a></li>' +
-          
-                      // Opción Trazabilidad
-                      '<li><a data-id="' + full['id_lote_envasado'] + '" data-bs-toggle="modal" class="dropdown-item edit">' +
-                      '<i class="ri-file-settings-line ri-20px text-warning"></i> Trazabilidad</a></li>' +
-          
-                      // Opción Eliminar lotes envasados
-                      '<li><a data-id="' + full['id_lote_envasado'] + '" class="dropdown-item delete-record text-danger">' +
-                      '<i class="ri-delete-bin-7-line ri-20px text-danger"></i> Eliminar lotes envasados</a></li>' +
-                    '</ul>' +
-                  '</div>' +
-                '</div>'
-              );
+
+
+                return (
+                    '<div class="d-flex align-items-center gap-50">' +
+                    '<button class="btn btn-sm btn-info dropdown-toggle hide-arrow" data-bs-toggle="dropdown"><i class="ri-settings-5-fill"></i>&nbsp;Opciones <i class="ri-arrow-down-s-fill ri-20px"></i></button>' +
+                    '<div class="dropdown-menu dropdown-menu-end m-0">' +
+                    `<a data-id="${full['id_lote_envasado']}" data-bs-toggle="modal" data-bs-target="#editLoteEnvasado" href="javascript:;" class="dropdown-item edit-record"><i class="ri-edit-box-line ri-20px text-info"></i>Editar lotes envasado</a>` +
+                    `<a data-id="${full['id_lote_envasado']}" data-bs-toggle="modal" data-bs-target="#reclasificacion" href="javascript:;" class="dropdown-item edit-reclasificacion"><i class="ri-id-card-fill ri-20px text-success"></i> Reclasificación SKU</a>` +
+                    `<a data-id="${full['id_lote_envasado']}" data-bs-toggle="modal" data-bs-target="#" href="javascript:;" class="dropdown-item "><i class="ri-git-repository-line ri-20px text-warning"></i> Trazabilidad</a>` +
+                    `<a data-id="${full['id_lote_envasado']}" class="dropdown-item delete-record  waves-effect text-danger"><i class="ri-delete-bin-7-line ri-20px text-danger"></i> Eliminar lotes envasados</a>` +
+                    /*               `<button class="btn btn-sm btn-icon edit-record btn-text-secondary rounded-pill waves-effect" data-id="${full['id_guia']}" data-bs-toggle="modal" data-bs-target="#editGuias"><i class="ri-edit-box-line ri-20px text-info"></i></button>` +
+                                  `<button class="btn btn-sm btn-icon delete-record btn-text-secondary rounded-pill waves-effect" data-id="${full['id_guia']}"><i class="ri-delete-bin-7-line ri-20px text-danger"></i></button>` + */
+                    '<div class="dropdown-menu dropdown-menu-end m-0">' +
+                    '<a href="' +
+                    userView +
+                    '" class="dropdown-item">View</a>' +
+                    '<a href="javascript:;" class="dropdown-item">Suspend</a>' +
+                    '</div>' +
+                    '</div>'
+                  );
+
             }
           }
           
