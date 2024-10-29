cfe04bbc054c98dac14a75a49dc21d7637d9d4e9
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2024-10-29 11:24:42 -0600

    Correcciones

--- resources/views/pdfs/DictamenProductor.blade.php
+++ resources/views/pdfs/DictamenProductor.blade.php
@@ -6,7 +6,136 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>F-UV-02-04 Ver 10, Dictamen de cumplimiento de Instalaciones como productor</title>
     <style>
-       
+        @font-face {
+            font-family: 'Lucida Sans Unicode';
+            src: url('fonts/lsansuni.ttf') format('truetype');
+        }
+
+        @font-face {
+            font-family: 'Lucida Sans Seminegrita';
+            src: url('fonts/LSANSD.ttf') format('truetype');
+        }
+
+        @font-face {
+            font-family: 'Arial Negrita';
+            src: url('fonts/arial-negrita.ttf') format('truetype');
+        }
+
+        body {
+            font-family: 'Lucida Sans Unicode';
+            margin-left: 20px; 
+            margin-right: 20px; 
+        }
+
+        .header {
+            margin-top: -30px; 
+            width: 100%;
+        }
+
+        .header img {
+            display: block;
+            width: 275px; 
+        }
+
+        .description1,
+        .description2,
+        .description3,
+        .textimg {
+            position: absolute;
+            right: 10px;
+            text-align: right;
+        }
+
+        #encabezado #titulos {
+            margin-top: 40px; /* Ajusta el espacio entre los párrafos */
+          
+        }
+
+        #encabezado p {
+            margin-bottom: 1px; /* Ajusta el espacio entre los párrafos */
+        }
+
+        #encabezado p:last-child {
+            margin-bottom: 0; /* Elimina el margen inferior del último párrafo */
+        }
+
+
+        #encabezado #logo{
+            
+            text-align: left;
+            float: left;
+        }
+
+        .description1 {
+            font-size: 18px;
+            color: #151442;
+            font-family: 'Arial Negrita' !important;
+            top: 5px; 
+        }
+
+        .description2 {
+            color: #151442;
+            font-family: 'Arial Negrita' !important;
+            font-size: 9.5px;
+            top: 30px; 
+        }
+
+        .description3 {
+            font-size: 10px;
+            top: 42px; 
+            margin-right: 40px; 
+        }
+
+        .textimg {
+            font-weight: bold;
+            position: absolute; 
+            top: 100px;
+            left: 10px; 
+            text-align: left;
+            font-size: 13px;
+        }
+
+        .text {
+            text-align: justify;
+            font-size: 13px;
+            line-height: 0.7;
+            margin-right: 10px;
+        }
+        
+        .text1 {
+            text-align: justify;
+            font-size: 11px;
+            line-height: 0.7;
+        }
+
+        .textp {
+            text-align: justify;
+            font-size: 13px;
+            line-height: 0.7;
+            margin-right: 10px;
+            font-family: 'Lucida Sans Seminegrita';
+        }
+
+        .title {
+            text-align: center;
+            font-size: 22px;
+            font-family: 'Arial Negrita', Gadget, sans-serif;
+            line-height: 20px;
+            margin-top: 10px; 
+        }
+
+        table {
+            width: 100%;
+            border: 2px solid #1E4678;
+            border-collapse: collapse;
+            margin: auto;
+            font-size: 12px;
+            line-height: 1;
+            vertical-align: top;
+            font-family: Arial, Helvetica, Verdana;
+        }
+
+    
     </style>
 </head>
 <body>
