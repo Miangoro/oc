0592fa600845a85c8be3ef09212a2bb3318f477d
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2024-11-04 12:15:28 -0600

    Correcciones

--- resources/views/pdfs/Pre-registro_predios.blade.php
+++ resources/views/pdfs/Pre-registro_predios.blade.php
@@ -6,7 +6,7 @@
     <title>F-UV-21-01 Pre-registro de predios de maguey o agave Ed.1 Vigente</title>
 
     <style>
-    @font-face {
+  /*  @font-face {
         font-family: 'calibri';
         src: url('fonts/calibri-regular.ttf') format('truetype');
     }
@@ -16,7 +16,11 @@
         src: url('fonts/calibri-bold.ttf') format('truetype');
     }
 
- 
+    @font-face {
+        font-family: 'century';
+        src: url('fonts/Century Schoolbook Bold.otf') format('truetype');
+    }*/
+
     body {
         margin: 0;
         padding: 0;
