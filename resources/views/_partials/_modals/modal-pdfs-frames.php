37c46c59b97c75d9d2629ec9843ad634e8dc757f
Ramses-Rojas <ches.rojas.torres@gmail.com>
2024-11-27 18:19:28 -0600

    Modal PDFs

--- resources/views/_partials/_modals/modal-pdfs-frames.php
+++ resources/views/_partials/_modals/modal-pdfs-frames.php
@@ -1,13 +1,4 @@
-<!--MODAL DICTAMEN INSTALACIONES PDF
-<div class="modal fade" id="mostrarPdf" tabindex="-1" aria-hidden="true">
-  <div class="modal-dialog modal-xl modal-simple">
-    <div class="modal-content">
-      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      <div class="modal-body p-0">
-        <div class="text-center mb-6">
-          <h4 id="titulo_modal" class="address-title mb-2"></h4>
-          <p id="subtitulo_modal" class="address-subtitle"></p>
-        </div>-->
+<!--MODAL GENERAL PDF's-->
 <div class="modal fade" id="mostrarPdf" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-xl modal-simple">
     <div class="modal-content">
