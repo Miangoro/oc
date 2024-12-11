e9ed8189b9110853cee208dd4fc4f91eab4860a3
Yexqls <yucli.emmanuel.666@gmail.com>
2024-12-10 22:10:56 -0600

    vista muestreo lote granel

--- resources/views/_partials/_modals/modal-add-solicitud-muestreo-lote-agranel.blade.php
+++ resources/views/_partials/_modals/modal-add-solicitud-muestreo-lote-agranel.blade.php
@@ -12,8 +12,8 @@
                     <div class="row">
                         <div class="col-md-6">
                             <div class="form-floating form-floating-outline mb-6">
-                                <select id="id_empresa_muestreo" onchange="obtenerInstalaciones();"
-                                    name="id_empresa" class="id_empresa_muestreo select2 form-select" required>
+                                <select id="id_empresa_muestreo" onchange="obtenerInstalaciones();" name="id_empresa"
+                                    class="id_empresa_muestreo select2 form-select" required>
                                     <option value="" disabled selected>Selecciona cliente</option>
                                     @foreach ($empresas as $empresa)
                                         <option value="{{ $empresa->id_empresa }}">{{ $empresa->razon_social }}
@@ -31,31 +31,103 @@
                             </div>
                         </div>
                     </div>
-                    
+
 
                     <div class="row">
                         <div class="col-md-12">
                             <div class="form-floating form-floating-outline mb-6 input-group ">
-                                <select class=" form-select" id="id_instalacion_muestreo" name="id_instalacion" aria-label="id_instalacion" required>
+                                <select class=" form-select" id="id_instalacion_muestreo" name="id_instalacion"
+                                    aria-label="id_instalacion" required>
                                     <option value="" selected>Lista de instalaciones</option>
                                     <!-- Aquí se llenarán las opciones con instalaciones del cliente -->
                                 </select>
-                               
-                                <button type="button" class="btn btn-primary" id="abrirModalInstalaciones"><i class="ri-add-line"></i> Agregar nueva instalación</button>
-                                
+                                <button type="button" class="btn btn-primary" id="abrirModalInstalaciones"><i
+                                        class="ri-add-line"></i> Agregar nueva instalación</button>
                             </div>
+                        </div>
                     </div>
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-floating form-floating-outline mb-4">
+                                <select onchange="obtenerDatosGraneles();" id="id_lote_granel" name="id_lote_granel"
+                                    class="select2 form-select">
+                                    <option value="">Selecciona lote a granel</option>
+                                    @foreach ($LotesGranel as $lotesgra)
+                                        <option value="{{ $lotesgra->id_lote_granel }}">{{ $lotesgra->nombre_lote }}
+                                        </option>
+                                    @endforeach
+                                </select>
+                                <label for="id_lote_granel">Lote a granel</label>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-floating form-floating-outline mb-6">
+                                <select id="destino_lote" name="destino_lote" class="form-select">
+                                    <option value="" disabled selected>Selecciona un tipo</option>
+                                    <option value="1">Nacional</option>
+                                    <option value="2">Exportación</option>
+                                </select>
+                                <label for="destino_lote">Tipo</label>
+                            </div>
+                        </div>
+
                     </div>
-                    
+                    <div class="row">
+                        <div class="col-md-4">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa una Categoria" />
+                                <label for="analisis">Ingresa Categoria</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa una Clase" />
+                                <label for="analisis">Ingresa Clase</label>
+                            </div>
+                        </div>
+                        <div class="col-md-4">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa un tipo de Maguey" />
+                                <label for="analisis">Ingresa Tipo de Maguey</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-5">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa Análisis fisicoquímico" />
+                                <label for="analisis">Ingresa Análisis fisicoquímico</label>
+                            </div>
+                        </div>
+                        <div class="col-md-2">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa % Alc. Vol." />
+                                <label for="analisis">% Alc. Vol.</label>
+                            </div>
+                        </div>
+                        <div class="col-md-5">
+                            <div class="form-floating form-floating-outline mb-5">
+                                <input type="text" class="form-control" id="analisis" name="analisis"
+                                    placeholder="Ingresa el Certificado de NOM a granel" />
+                                <label for="analisis">Ingresa Certificado de NOM a granel</label>
+                            </div>
+                        </div>
+                    </div>
                     <div class="row">
                         <div class="form-floating form-floating-outline mb-5">
-                            <textarea name="info_adicional" class="form-control h-px-150" id="comentarios" placeholder="Información adicional sobre la actividad..."></textarea>
+                            <textarea name="info_adicional" class="form-control h-px-150" id="comentarios"
+                                placeholder="Información adicional sobre la actividad..."></textarea>
                             <label for="comentarios">Información adicional sobre la actividad</label>
                         </div>
                     </div>
                     <div class="col-12 mt-6 d-flex flex-wrap justify-content-center gap-4 row-gap-4">
                         <button type="submit" class="btn btn-primary">Registrar</button>
-                        <button  type="reset" class="btn btn-outline-secondary btnCancelar" data-bs-dismiss="modal"
+                        <button type="reset" class="btn btn-outline-secondary btnCancelar" data-bs-dismiss="modal"
                             aria-label="Close">Cancelar</button>
                     </div>
                 </form>
@@ -77,23 +149,23 @@
             success: function(response) {
                 console.log(response);
                 var contenido = "";
-            for (let index = 0; index < response.instalaciones.length; index++) {
-                // Limpia el campo tipo usando la función limpiarTipo
-                var tipoLimpio = limpiarTipo(response.instalaciones[index].tipo);
+                for (let index = 0; index < response.instalaciones.length; index++) {
+                    // Limpia el campo tipo usando la función limpiarTipo
+                    var tipoLimpio = limpiarTipo(response.instalaciones[index].tipo);
 
-                contenido = '<option value="' + response.instalaciones[index].id_instalacion + '">' +
-                    tipoLimpio + ' | ' + response.instalaciones[index].direccion_completa + '</option>' +
-                    contenido;
-            }
-            if (response.instalaciones.length == 0) {
-                contenido = '<option value="">Sin instalaciones registradas</option>';
-            }
-            $('#id_instalacion_muestreo').html(contenido);
+                    contenido = '<option value="' + response.instalaciones[index].id_instalacion + '">' +
+                        tipoLimpio + ' | ' + response.instalaciones[index].direccion_completa +
+                        '</option>' +
+                        contenido;
+                }
+                if (response.instalaciones.length == 0) {
+                    contenido = '<option value="">Sin instalaciones registradas</option>';
+                }
+                $('#id_instalacion_muestreo').html(contenido);
             },
             error: function() {
                 //alert('Error al cargar los lotes a granel.');
             }
         });
     }
-
 </script>
