50c6e94d49217761ae788d5c37f9150c654e9bb4
Carlos Zarco <cardrooiid@gmail.com>
2024-12-11 16:56:41 -0600

    bitacora v1

---
+++ resources/views/pdfs/Bitacora_Mezcal.blade.php
@@ -0,0 +1,354 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Bitácora Mezcal a Granel</title>
+</head>
+<style>
+    /* General styling for the table */
+    table {
+        width: 100%; /* Make the table span across the entire page */
+        border-collapse: collapse; /* Remove spaces between borders */
+        table-layout: fixed; 
+    }
+
+    /* Style for table cells and headers */
+    th, td {
+        border: 1px solid #595959;
+        padding: 8px;
+        text-align: center;
+        font-size: 10px;
+        word-wrap: break-word;
+    }
+
+    /* Style for table headers */
+    th {
+        background-color: #f0e6cc;
+        font-weight: bold;
+    }
+
+    /* Alternating row colors */
+    tr:nth-child(even) {
+        background-color: #fbf8f0;
+    }
+
+    tr:nth-child(odd) {
+        background-color: #fefcf9;
+    }
+
+    /* Adjust the width of columns */
+    td, th {
+        width: auto; /* Allow content to decide width */
+        height: 25px;
+    }
+
+    /* Responsive behavior for smaller screens */
+    @media screen and (max-width: 768px) {
+        table, th, td {
+            font-size: 8px; /* Smaller text on mobile */
+        }
+    }
+</style>
+<body>
+    
+    <table>
+        <tbody>
+            <tr>
+                <td rowspan="2">FECHA</td>
+                <td rowspan="2">ID DE TANQUE</td>
+                <td rowspan="2">LOTE A GRANEL</td>
+                <td rowspan="2">OPERACIÓN ADICIONAL</td>
+                <td rowspan="2">CATEGORÍA</td>
+                <td rowspan="2">CLASE</td>
+                <td rowspan="2">INGREDIENTE(S)</td>
+                <td rowspan="2">EDAD (AÑOS)</td>
+                <td rowspan="2">TIPO DE AGAVE</td>
+                <td rowspan="2">NÚM. ANALÍSIS FISICOQUIÍMICO</td>
+                <td rowspan="2">NÚM. DE CERTIFICADO</td>
+                <td colspan="2">INVENTARIO INICIAL</td>
+                <td colspan="4">ENTRADA</td>
+                <td colspan="3">SALIDAS</td>
+                <td colspan="2">INVENTARIO FINAL</td>
+                <td rowspan="2">OBSERVACIONES</td>
+                <td rowspan="2">FIRMA DE LA UI</td>
+            </tr>
+            <tr>
+                <td>VOLUMEN</td>
+                <td>%ALC.VOL.</td>
+                <td>PROCEDENCIA</td>
+                <td>VOLUMEN</td>
+                <td>%ALC.VOL.</td>
+                <td>AGUA</td>
+                <td>VOLUMEN</td>
+                <td>%ALC.VOL.</td>
+                <td>DESTINO</td>
+                <td>VOLUMEN</td>
+                <td>%ALC.VOL.</td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+            <tr>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+                <td></td>
+            </tr>
+        </tbody>
+    </table>
+
+</body>
+</html>
