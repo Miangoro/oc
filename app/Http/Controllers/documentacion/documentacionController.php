a7a83c5a543916414e409642110cabefe4fea322
Yexqls <yucli.emmanuel.666@gmail.com>
2024-12-18 11:12:41 -0600

    fix img documentos

--- app/Http/Controllers/documentacion/documentacionController.php
+++ app/Http/Controllers/documentacion/documentacionController.php
@@ -432,7 +432,7 @@ print_r($instalaciones->getBindings());*/
   <!-- Top Referral Source Mobile  -->
   <div class="col-xxl-12">
     <div class="card"> 
-      <img src="' . asset('assets/img/branding/banner_documentos.png') . '" alt="timeline-image" class="card-img-top h-px-200" style="object-fit: cover;">
+      <img src="' . asset('assets/img/branding/banner_documentos.png') . '" alt="timeline-image" class="card-img-top img-fluid" style="object-fit: cover;">
       <div class="card-header d-flex justify-content-between">
         <div>
           <h5 class="card-title mb-1">' . $numeroCliente . ' ' . $razonSocial . ' (' . $norma . ')</h5>
