f131aa3f1480e18353b395dfa1f731b85e14cc33
Carlos Zarco <cardrooiid@gmail.com>
2024-12-20 15:55:10 -0600

    bitacora

--- resources/views/_partials/_modals/modal-add-bitacoraMezcal.blade.php
+++ resources/views/_partials/_modals/modal-add-bitacoraMezcal.blade.php
@@ -1,7 +1,11 @@
 <div class="modal fade" id="RegistrarBitacoraMezcal" tabindex="-1" aria-labelledby="registroInventarioModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
-        
+            <div class="modal-header">
+                <h5 class="modal-title" id="addBitacora">Agregar Bitácora</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+
             <div class="modal-body">
                 <form id="registroInventarioForm" action="/ruta-de-envio" method="POST">
                     @csrf
