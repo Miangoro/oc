5a8c5dc46f4f20466dbf3852d217db0738fbb56d
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-03-11 12:16:06 -0600

    vista datos cliente cards bonitos

--- resources/views/clientes/find_clientes_prospecto_view.blade.php
+++ resources/views/clientes/find_clientes_prospecto_view.blade.php
@@ -63,7 +63,7 @@
           </div>
 
           <!-- CARDS-->
-          <div class="form-floating-outline m-2 row">
+          {{-- <div class="form-floating-outline m-2 row">
             
             <div class="col-md-6 col-xl-4">
               <div class="card bg-primary text-white">
@@ -86,7 +86,7 @@
               </div>
             </div>
 
-          </div><!--FIN CARDS-->
+          </div> --}}<!--FIN CARDS-->
 
 
 
@@ -111,21 +111,47 @@
                         
                         <!-- Contenido que se despliega -->
                         <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionCard1">
-                            <div class="accordion-body">
-                              @php
-                                  $counter = 1;  
-                              @endphp
+                            {{-- <div class="accordion-body">
                                 @if($empresa && $empresa->instalaciones->count() > 0)
                                     @foreach($empresa->instalaciones as $instalacion)
-                                        {{ $counter }}. {{ $instalacion->direccion_completa }}<br><br>
-                                      @php
-                                          $counter++;
-                                      @endphp
+                                      {{ $instalacion->direccion_completa }}<br><br>
                                     @endforeach
                                 @else
                                     <p>No hay instalaciones registradas para esta empresa.</p>
                                 @endif
-                            </div>
+                            </div> --}}
+
+                            @if($empresa && $empresa->instalaciones->count() > 0)
+                                <div class="row mt-3">
+                                    <!-- Tarjeta para cada marca dentro del contenido desplegable -->
+                                    @foreach($empresa->instalaciones as $instalacion)
+                                        <div class="col-lg-6 mb-5" style="font-size: 14px">
+                                            <div class="card bg-label-primary">
+                                                <div class="card-body">
+                                                    <p class="card-title"><b>
+
+                                                    @php
+                                                      $tipos = json_decode($instalacion->tipo, true); // Decodifica el JSON a un array
+                                                    @endphp
+                                                    @if(!empty($tipos))
+                                                        {{ implode(', ', $tipos) }}  {{-- Une todos los tipos con una coma y un espacio --}}
+                                                    @else
+                                                        Sin Tipo
+                                                    @endif
+                                                    
+                                                    </b></p>
+                                                    <p class="card-text">
+                                                      Direccion: {{ $instalacion->direccion_completa }} <span class="d-block mt-2">
+                                                      </p>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    @endforeach
+                                  @else
+                                      <p>No hay instalaciones registradas para esta empresa.</p>
+                                  @endif
+                                </div>
+                            
                         </div>
                     </div>
                 </div>
@@ -161,14 +187,16 @@
         
                             <!-- Añadir tarjetas dentro del contenido desplegable de marcas -->
                             @if($empresa && $empresa->marcas->count() > 0)
-                                <div class="row mt-4">
+                                <div class="row mt-3">
                                     <!-- Tarjeta para cada marca dentro del contenido desplegable -->
                                     @foreach($empresa->marcas as $marca)
-                                        <div class="col-md-6 col-xl-4 mb-3">
-                                            <div class="card">
+                                        <div class="col-lg-6 mb-5" style="font-size: 14px">
+                                            <div class="card bg-label-primary">
                                                 <div class="card-body">
-                                                    <p class="card-title" style="font-size: 15px"><b>{{ $marca->marca }}</b></p>
-                                                    <p class="card-text">Folio: {{ $marca->folio }}</p>
+                                                    <p class="card-title"><b>{{ $marca->marca }}</b></p>
+                                                    <p class="card-text">
+                                                      Folio: {{ $marca->folio }} <span class="d-block mt-2">
+                                                      Norma: {{ $marca->catalogo_norma_certificar->norma }}</p>
                                                 </div>
                                             </div>
                                         </div>
@@ -179,7 +207,48 @@
                     </div>
                 </div>
             </div>
-        </div>
+
+            <!-- TARJETA 3 -->
+            <div class="col-md-6 col-xl-4 mb-3">
+              <div class="accordion" id="accordionCard3">
+                  <div class="accordion-item">
+                      <!-- Título de la tarjeta como el encabezado del acordeón -->
+                      <h5 class="accordion-header" id="titulo3">
+                          <button class="accordion-button text-white {{ $empresa && $empresa->marcas->count() > 0 ? 'bg-primary' : 'bg-danger' }}" type="button" data-bs-toggle="collapse" data-bs-target="#coleccion3" aria-expanded="false" aria-controls="coleccion3">
+                              MARCAS ({{ $empresa->marcas->count() }})<br><br>
+                              @if($empresa && $empresa->marcas->count() > 0)
+                                  Despliega para ver las marcas
+                              @else
+                                  Sin registros
+                              @endif
+                          </button>
+                      </h5>
+                      <!-- Contenido que se despliega -->
+                      <div id="coleccion3" class="accordion-collapse collapse" aria-labelledby="titulo3" data-bs-parent="#accordionCard3">
+                          <!-- Añadir tarjetas dentro del contenido desplegable de marcas -->
+                          @if($empresa && $empresa->marcas->count() > 0)
+                              <div class="row mt-3">
+                                  <!-- Tarjeta para cada marca dentro del contenido desplegable -->
+                                  @foreach($empresa->marcas as $marca)
+                                    <div class="col-lg-6 mb-5" style="font-size: 14px">
+                                        <div class="card bg-label-primary">
+                                            <div class="card-body">
+                                                <p class="card-title"><b>{{ $marca->marca }}</b></p>
+                                                <p class="card-text">
+                                                  Folio: {{ $marca->folio }} <span class="d-block mt-2">
+                                                  Norma: {{ $marca->catalogo_norma_certificar->norma }}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                  @endforeach
+                              </div>
+                          @endif
+                      </div>
+                  </div>
+              </div>
+          </div>
+        </div><!--FIN-->
         
        
           
