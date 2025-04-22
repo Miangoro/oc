5322c9c82f59696fcd24c58c7e260a94df6f9408
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-04-22 15:20:14 -0600

    Cambios en la revisión de personal

--- resources/views/pdfs/pdf_bitacora_revision_personal.blade.php
+++ resources/views/pdfs/pdf_bitacora_revision_personal.blade.php
@@ -389,7 +389,7 @@
                     $firmaPath = $firmaRevisor ? 'firmas/' . $firmaRevisor : null;
                 @endphp
                 @if ($firmaRevisor && Storage::disk('public')->exists($firmaPath))
-                    <img style="position: absolute; top: 868px; left: 125; right: 0; margin: 0 auto;" height="50px"
+                    <img style="position: absolute; top: 868px; left: 140; right: 0; margin: 0 auto;" height="50px"
                         src="{{ asset('storage/' . $firmaPath) }}">
                 @endif
                 DE QUIEN TOMA LA APROBACIÓN</td>
