1272ad91f4b1d658b796a736caef6ceab713db29
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-05-02 13:29:33 -0600

    Corrección en pedidos para exportación

--- app/Models/lotesGranel.php
+++ app/Models/lotesGranel.php
@@ -100,9 +100,10 @@ class LotesGranel extends Model
 
 
     public function certificadoGranel()
-    {
-        return $this->hasOne(CertificadosGranel::class, 'id_lote_granel','id_lote_granel');
-    }
+{
+    return $this->hasOne(CertificadosGranel::class, 'id_lote_granel', 'id_lote_granel');
+}
+
     
 
 
