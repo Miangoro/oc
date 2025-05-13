0ab7d17bde351f35246cee016e3803d63f436f8a
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-05-12 18:12:42 -0600

    certificado expor. Vo. Bo

--- resources/js/certificados_exportacion.js
+++ resources/js/certificados_exportacion.js
@@ -318,6 +318,7 @@ if (dt_user_table.length) {
                   '<div class="dropdown-menu dropdown-menu-end m-0">' +
                     `<a data-id="${full['id_certificado']}" data-bs-toggle="modal" data-bs-target="#ModalEditar" href="javascript:;" class="dropdown-item text-dark editar"> <i class="ri-edit-box-line ri-20px text-info"></i> Editar</a>` +
                     `<a data-id="${full['id_certificado']}" data-bs-toggle="modal" data-bs-target="#asignarRevisorModal" class="dropdown-item waves-effect text-dark"> <i class="text-warning ri-user-search-fill"></i> Asignar revisor </a>` +
+                    `<a data-id="${full['id_certificado']}" data-bs-toggle="modal" data-bs-target="#Modal" href="javascript:;" class="dropdown-item text-dark"> <i class="ri-edit-box-line ri-20px text-light"></i> Vo. Bo. Cliente</a>` +
                     `<a data-id="${full['id_certificado']}" data-folio="${full['num_certificado']}" data-bs-toggle="modal" data-bs-target="#ModalTracking"  class="dropdown-item waves-effect text-black trazabilidad"> <i class="ri-history-line text-secondary"></i> Trazabilidad</a>` +
                     `<a data-id="${full['id_certificado']}" data-bs-toggle="modal" data-bs-target="#modalAddReexCerExpor" class="dropdown-item waves-effect text-black reexpedir"> <i class="ri-file-edit-fill text-success"></i> Reexpedir/Cancelar</a>` +
                     `<a data-id="${full['id_certificado']}" class="dropdown-item waves-effect text-black eliminar"> <i class="ri-delete-bin-7-line ri-20px text-danger"></i> Eliminar</a>` +
