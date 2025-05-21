25140eee9879243254fcc9acf340740f2d0c9871
Jorge <jorgebravomendez6563@gmail.com>
2025-05-21 14:53:56 -0600

    Avance con lo de excel solo falta agregar los campos

--- app/Http/Controllers/certificados/Certificado_ExportacionController.php
+++ app/Http/Controllers/certificados/Certificado_ExportacionController.php
@@ -18,6 +18,9 @@ use App\Models\solicitudesModel;
 use Barryvdh\DomPDF\Facade\Pdf;
 use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+//Clase de exportacion
+use Maatwebsite\Excel\Facades\Excel;
+use App\Exports\CertificadosExport;
 
 
 class Certificado_ExportacionController extends Controller
@@ -206,7 +209,16 @@ public function index(Request $request)
     ]);
 }
 
-
+public function exportar(Request $request)
+{
+    try {
+        $filtros = $request->only(['id_empresa', 'anio', 'estatus', 'mes']);
+        return Excel::download(new CertificadosExport($filtros), 'reporte_certificados.xlsx');
+    } catch (\Exception $e) {
+        \Log::error('Error al generar el reporte: ' . $e->getMessage());
+        return response()->json(['message' => 'Error al generar el reporte. Verifica los filtros e intenta nuevamente.', 'code' => 500]);
+    }
+}
 
 ///FUNCION REGISTRAR
 public function store(Request $request)
@@ -684,6 +696,7 @@ public function MostrarSolicitudCertificadoExportacion($id_certificado)
     //nombre al descargar
     return $pdf->stream('Solicitud de emisión de Certificado Combinado para Exportación NOM-070-SCFI-2016 F7.1-01-55.pdf');
 }
+
 
 
 
