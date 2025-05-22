c4eb9a0d47fd015dd9699ebcb08f21e9b74460f9
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-05-22 17:33:03 -0600

    dictamen exportacino UMEXP##-### 2.3

--- app/Http/Controllers/dictamenes/DictamenExportacionController.php
+++ app/Http/Controllers/dictamenes/DictamenExportacionController.php
@@ -77,7 +77,7 @@ public function index(Request $request)
     
     // Columnas ordenadas desde DataTables
     $orderColumnIndex = $request->input('order.0.column');// Indice de columna en DataTables
-    $orderDirection = $request->input('order.0.dir') ?? 'asc';// Dirección de ordenamiento
+    $orderDirection = $request->input('order.0.dir') ?? 'desc';// Dirección de ordenamiento
     $orderColumn = $columns[$orderColumnIndex] ?? 'num_dictamen'; // Por defecto
     
     $search = $request->input('search.value');//Define la búsqueda global.
