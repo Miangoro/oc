b165b68ce7ec938754256eeccb8f78bc59b341f9
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-05-22 17:00:40 -0600

    diseño en dasboard

--- app/Http/Controllers/insertar_datos_bd_dictamenes_exportacion.php
+++ app/Http/Controllers/insertar_datos_bd_dictamenes_exportacion.php
@@ -65,9 +65,7 @@ class insertar_datos_bd_dictamenes_exportacion extends Controller
                         //if ($solicitud['n_servicio'] ==$inspecciones->n) {
                             $id_solicitud = Dictamen_Exportacion::create([
                                 'id_inspeccion'   => $inspecciones->id_inspeccion,
-                               
-                             
-                                'num_dictamen'    => preg_replace('/CIDAM C-EXP\d*/', 'UMEXP', $solicitud['n_certificado']),
+                                'num_dictamen' => preg_replace('/CIDAM C-EXP(-?\d+)(\/\d{4}(-[A-Z])?)?/','UMEXP$1',$solicitud['n_certificado']),
                                 'fecha_emision'   => $solicitud['fecha_expedicion'],
                                 'fecha_vigencia'  => $solicitud['fecha_vigencia'],
                                 'id_firmante'        => $id_firmante,
