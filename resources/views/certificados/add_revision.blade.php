7c50a76103bd73ed54d7f2b727e31c7f0ebe92e9
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-06-11 16:53:18 -0600

    combinado

--- resources/views/certificados/add_revision.blade.php
+++ resources/views/certificados/add_revision.blade.php
@@ -244,34 +244,34 @@
                                         @elseif($pregunta->filtro == 'lote_granel')
                                             <td><b>{{ $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->nombre_lote ?? 'N/A' }}</b>
                                             </td>
+
+                                            @php
+    $folioFq = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->folio_fq ?? '';
+
+    // Explota y limpia
+    $folios = collect(explode(',', $folioFq))
+                ->map(fn($f) => trim($f))
+                ->filter()
+                ->values();
+
+    $primerFolio = $folios->get(0, 'N/A');
+    $segundoFolio = $folios->get(1, 'N/A');
+@endphp
                                        @elseif($pregunta->filtro == 'nanalisis')
-                                            <td><b>{{ $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->folio_fq ?? 'N/A' }}</b>
-                                               
-                                                 @foreach ($datos->certificado->dictamen->inspeccione->solicitud->lote_granel->fqs as $documento)
-                                                        <a target="_blank"
-                                                            href="/files/{{ $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->empresa->empresaNumClientes->firstWhere(
-                                                                'numero_cliente',
-                                                                '!=',
-                                                                null,
-                                                            )->numero_cliente }}/fqs/{{ $documento->url }}">
-                                                            <i
-                                                                class="ri-file-pdf-2-fill text-danger ri-40px pdf cursor-pointer"></i>
-                                                        </a>
-                                                    @endforeach
-                                            </td>
+    <td>
+        <b>{{ $primerFolio }}</b>
+
+        @foreach ($datos->certificado->dictamen->inspeccione->solicitud->lote_granel->fqs as $documento)
+            <a target="_blank"
+                href="/files/{{ $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->empresa->empresaNumClientes->firstWhere('numero_cliente', '!=', null)->numero_cliente }}/fqs/{{ $documento->url }}">
+                <i class="ri-file-pdf-2-fill text-danger ri-40px pdf cursor-pointer"></i>
+            </a>
+        @endforeach
+    </td>
+
                                        @elseif($pregunta->filtro == 'nanalisis_ajuste')
-                                             @php
-        $folioFq = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->folio_fq ?? null;
-
-        // Limpieza y separación
-        $folios = collect(explode(',', $folioFq))
-                    ->map(fn($f) => trim($f))
-                    ->filter()
-                    ->values();
-
-        $segundoFolio = $folios->count() > 1 ? $folios->get(1) : 'N/A';
-    @endphp
     <td><b>{{ $segundoFolio }}</b></td>
+
                                         @elseif($pregunta->filtro == 'aduana')
                                             <td><b>
                                                     {{ json_decode($datos->certificado->dictamen->inspeccione->solicitud->caracteristicas, true)['aduana_salida'] ?? 'N/A' }}
