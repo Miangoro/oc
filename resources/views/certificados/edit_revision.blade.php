25e238389a09fc140c4a21befda45de46c04a58c
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-02 11:11:56 -0600

    cambio en rev consejo

--- resources/views/certificados/edit_revision.blade.php
+++ resources/views/certificados/edit_revision.blade.php
@@ -43,6 +43,12 @@
                     <div>
                         <p class="text-muted mb-1">Tipo de certificado</p>
                         <h5 class="fw-semibold mb-2">{{ $tipo }}</h5>
+                         @php
+                            $caracteristicas = json_decode( $datos->certificado->dictamen->inspeccione->solicitud->caracteristicas);
+                        @endphp
+                        @if (isset($caracteristicas->tipo_solicitud) && $caracteristicas->tipo_solicitud === '2')
+                            <span class="badge bg-info">Combinado</span>
+                        @endif
                         @if ($datos->es_correccion === 'si')
                             <span class="badge bg-danger">Es corrección</span>
                         @endif
