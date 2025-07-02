51a6e67acede5a1e3baccc720a324c9e4fdff029
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-07-02 16:54:22 -0600

    modales duplicados  de PDF ocultos

--- resources/views/_partials/_modals/modal-pdfs-frames.php
+++ resources/views/_partials/_modals/modal-pdfs-frames.php
@@ -40,9 +40,9 @@
 
 
 
-
+<!--OCULTADOS EL 02-JULIO-2025, si no afecta en algun lado, borrarlos-->
 <!--/  Modal para dictamenes grnel-->
-<div class="modal fade" id="mostrarPdfDictamen" tabindex="-1" aria-hidden="true">
+<!-- <div class="modal fade" id="mostrarPdfDictamen" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-xl modal-simple">
     <div class="modal-content">
       <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
@@ -73,17 +73,17 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 
 <!--/  Modal para ???-->
-<div class="modal fade" id="mostrarPdfDictamen1" tabindex="-1" aria-hidden="true">
+<!-- <div class="modal fade" id="mostrarPdfDictamen1" tabindex="-1" aria-hidden="true">
   <div class="modal-dialog modal-xl modal-simple">
     <div class="modal-content">
     <a id="openPdfBtnDictamen1" href="#" target="_blank" class="btn btn-primary btn-sm ms-auto" style="display: none;">Abrir PDF en nueva pestaña</a>
       <div class="modal-header d-flex justify-content-center">
         <h4 id="titulo_modal_Dictamen1" style="text-align: center; flex-grow: 1;"></h4>
-        <!-- Botón de cierre más pequeño y alineado a la derecha -->
+        //Botón de cierre más pequeño y alineado a la derecha
       </div><button type="button" class="btn-close btn-sm ml-auto" data-bs-dismiss="modal" aria-label="Close"></button>
       <div class="modal-body p-0 d-flex flex-column justify-content-center align-items-center">
         <div class="text-center mb-6">
@@ -110,7 +110,7 @@
       </div>
     </div>
   </div>
-</div>
+</div> -->
 
 
 
