db85ee7c98d6c25802c9d64f01abbe3c6b10e6ec
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-04 11:04:50 -0600

    cambio en rev consejo

--- resources/views/pdfs/certificado_exportacion_ed13.blade.php
+++ resources/views/pdfs/certificado_exportacion_ed13.blade.php
@@ -524,7 +524,7 @@
     <p class="textx" style="margin-top: 5px">
         <strong>AUTORIZÓ</strong>
         <span style="margin-left: 54px; display: inline-block; text-align: left; position: relative; margin-top: 10px;">
-            <strong>{{ $data->firmante->puesto ?? '' }}<br><br>{{ $data->firmante->name ?? '' }}</strong>
+            <strong>{{ $data->firmante->puesto ?? '' }}<br><br>{{ mb_strtoupper($data->firmante->name ?? '', 'UTF-8') }}</strong>
         </span>
     </p>
     <p class="textx">
