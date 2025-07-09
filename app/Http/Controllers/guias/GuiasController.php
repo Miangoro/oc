2355bb727d34797e7274f2fc638490ddc7c0d91a
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-07-09 10:40:04 -0600

    guias

--- app/Http/Controllers/guias/GuiasController.php
+++ app/Http/Controllers/guias/GuiasController.php
@@ -19,6 +19,7 @@ use Endroid\QrCode\QrCode;
 use Endroid\QrCode\RoundBlockSizeMode;
 use Endroid\QrCode\Writer\PngWriter;
 use Illuminate\Support\Facades\Auth;//sesion iniciada
+use Illuminate\Support\Facades\Storage;
 
 
 class GuiasController  extends Controller
@@ -85,7 +86,6 @@ class GuiasController  extends Controller
 
         $query = Guias::with(['empresa.empresaNumClientes', 'predios'])
             ->groupBy('run_folio');
-
         if ($empresaId) {
             $query->where('id_empresa', $empresaId);
         }
@@ -203,12 +203,35 @@ class GuiasController  extends Controller
     public function destroy($id_guia)
     {
         $guia = Guias::findOrFail($id_guia);
-    
         $run_folio = $guia->run_folio;
-    
-        Guias::where('run_folio', $run_folio)->delete();
-    
-        return response()->json(['success' => 'Todas las guías con el mismo run_folio se eliminaron correctamente']);
+
+        // Obtener todas las guías con ese run_folio
+        $guias = Guias::where('run_folio', $run_folio)->get();
+
+        foreach ($guias as $guia) {
+            // Buscar solo documentos con ID 71 (guía) o 132 (resultados ART)
+            $documentos = Documentacion_url::where('id_relacion', $guia->id_guia)
+                ->whereIn('id_documento', [71, 132])
+                ->get();
+
+            foreach ($documentos as $doc) {
+                //Busca el archivo fisico
+                $numeroCliente = $guia->empresa->empresaNumClientes->first()?->numero_cliente;
+                $rutaArchivo = 'uploads/' . $numeroCliente . '/' . $doc->url;
+                // Eliminar archivo físico si existe
+                if ($numeroCliente && Storage::disk('public')->exists($rutaArchivo)) {
+                    Storage::disk('public')->delete($rutaArchivo);
+                }
+
+                // Eliminar registro en la tabla documentacion_url
+                $doc->delete();
+            }
+
+            // Eliminar la guía
+            $guia->delete();
+        }
+
+        return response()->json(['success' => 'Guías y documentos con mismo run_folio eliminados correctamente.']);
     }
     
 
