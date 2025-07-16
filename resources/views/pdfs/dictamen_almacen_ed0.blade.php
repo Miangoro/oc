365a0b1b945b7eb58a2689545982908e5ee7443b
Cindy130901 <cindygpe18@gmail.com>
2025-07-16 14:41:03 -0600

    dictamen comercializador y almacen

--- resources/views/pdfs/dictamen_almacen_ed0.blade.php
+++ resources/views/pdfs/dictamen_almacen_ed0.blade.php
@@ -144,6 +144,82 @@
         padding-left: 1em;
         font-weight: bold;
     }
+     .numero {
+  font-weight: bold;
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+
+.numero span {
+  margin-left: 5px;
+}
+.titulo {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 2px;
+}
+
+.subtitulo {
+  font-size: 16px;
+  font-weight: bold;
+}
+.bloque-centrado {
+  text-align: center;
+  margin-bottom: 20px;
+  line-height: 1.4;
+}
+
+.bloque-centrado .numero,
+.bloque-centrado .titulo,
+.bloque-centrado .subtitulo {
+  margin: 4px 0;
+}
+.justificado {
+  text-align: justify;
+  font-size: 15px;
+  line-height: 1;
+}
+.resaltado {
+  font-weight: bold;
+  font-size: 16px;
+}
+.destinatario {
+  text-align: center;
+  font-size: 35px;
+  font-weight: bold;
+  color: #1f497c; /* Azul profesional */
+  text-transform: uppercase;
+  margin-top: 5px;
+}
+.datos-comercio {
+  text-align: justify;
+  font-size: 15px;
+  line-height: 1;
+  margin-top: 10px;
+  margin-bottom: 20px;
+} 
+.resaltado2 {
+  font-weight: bold;
+  font-size: 15px;
+}  
+.nota-final {
+  text-align: justify;
+  font-size: 13px;
+  color: #999999; /* Gris claro, más tenue que el texto normal */
+  line-height: 1;
+  margin-top: 10px;
+  margin-bottom: 40px;
+}
+.bloque-autorizacion {
+  margin-top: 30px;
+  line-height: 1;
+}
+
+.autorizado {
+  font-size: 11px;
+  font-family: 'Calibri', sans-serif;
+ 
+}
 
     </style>
 </head>
@@ -176,9 +252,56 @@
 
 <div class="contenido">
 
+<div class="bloque-centrado">
+    <div class="numero">No.: </div>
+    <div class="titulo"> Nombre del productor/empresa:  </div>
+    <div class="subtitulo">Dictamen de cumplimiento de Instalaciones de Bodega o Almacén</div>
+
+     <p class="justificado">De acuerdo a lo establecido en los procedimientos internos de la Unidad de Inspección <span class="resaltado">No.UVNOM129</span> de la
+ Universidad Michoacana de San Nicolás de Hidalgo, con domicilio en Francisco J. Múgicas/n Col. Felicitas del
+ Rio, Morelia, Michoacán para la revisión de procesos de producción del producto Mezcal, su envasado y
+ comercialización; y confundamento en los artículos 56 Fracción I y 60 fracción I de la Ley de Infraestructura de la
+ Calidad que establece el funcionamiento de las Unidades de Inspección. </p>
+</div>
+ <p class="justificado">Después de realizar la inspección de las
+ instalaciones en fecha del <u>10 de Junio del 2025</u> partiendo del acta circunstanciada o número de inspección:
+ <u>UMS-0725/2025</u> se otorga el Dictamen de Instalaciones</p>
+
+<div class="bloque-centrado">
+ <div class="numero">Nombre del productor/empresa:  </div>
+ </div>
+
+<div class="bloque-centrado">
+  <div class="destinatario">AMANTES DEL MEZCAL, S.A. DE</div>
+</div>
+
+<p class="datos-comercio">
+  <span class="resaltado2">Domicilio Fiscal: </span> AMANTES DEL MEZCAL, S.A.DEC.V. Avenida Ferrocarril, Número exterior 69, Número
+ interior Bis., San Sebastián Tutla, 71320, , Oaxaca De Juárez, Oaxaca<br>
+  <span class="resaltado2">Domicilio de la Bodega o Almacén: </span> Nacional, 85, -, San Dionisio Ocotepec, 70495, San Dionisio Ocotepec,
+ Oaxaca<br>
+  <span class="resaltado2">Responsable de inspección: </span><br>
+  <span class="resaltado2">Fecha de emisión del dictamen:</span>03 de Julio del 2025<br>
+  <span class="resaltado2"> Vigencia hasta:</span> 10 de Junio del 2026 Se dictamina que la Bodega o Almacén cuenta con la infraestructura, el
+ equipo y los procesos necesarios para la producción de <u>Mezcal Artesanal</u>, clase (s) <u>Blanco o Joven, Abocado con,
+ Destilado con</u>, requisitos establecidos en la NOM-070-SCFI-2016, Bebidas alcohólicas-Mezcal-Especificaciones y
+ por el Organismo de Certificación del Centro de Innovación y Desarrollo Agroalimentario de Michoacán A.C.
+ (CIDAM).
+</p>
+<p class="nota-final">Las instalaciones se encuentran en región de los estados y municipios que contempla la resolución mediante el cual se otorga la
+ protección prevista a la denominación de origen Mezcal, para ser aplicada a la bebida alcohólica del mismo nombre, publicada
+ el 28 de noviembre de 1994, así como sus modificaciones subsecuentes.</p>
+
+<div class="bloque-autorizacion">
+  <div class="autorizado">C.c.p Dirección General de Normas</div>
+  <div class="autorizado">C.c.p Gerente del Organismo Certificador del CIDAM A.C.</div>
+  <div class="autorizado">C.c.p Expediente de la Unidad de Verificación del UMSNH.</div>
+</div>
+
 
 
 </div>
+
 
 
 
--- resources/views/pdfs/dictamen_comercializador_ed9.blade.php
+++ resources/views/pdfs/dictamen_comercializador_ed9.blade.php
@@ -144,6 +144,84 @@
         padding-left: 1em;
         font-weight: bold;
     }
+    .numero {
+  font-weight: bold;
+  margin-bottom: 5px;
+  font-size: 18px;
+}
+
+.numero span {
+  margin-left: 5px;
+}
+
+.titulo {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 2px;
+}
+
+.subtitulo {
+  font-size: 16px;
+  font-weight: bold;
+}
+.bloque-centrado {
+  text-align: center;
+  margin-bottom: 20px;
+  line-height: 1.4;
+}
+
+.bloque-centrado .numero,
+.bloque-centrado .titulo,
+.bloque-centrado .subtitulo,
+.bloque-centrado .comercializador {
+  margin: 4px 0;
+}
+.comercializador {
+  font-size: 40px;
+  color: #a5a5a5; /* gris claro */
+  font-weight: bold;
+  margin-top: 5px;
+  text-align: center;
+}
+.justificado {
+  text-align: justify;
+  font-size: 15px;
+  line-height: 1;
+}
+.resaltado {
+  font-weight: bold;
+  font-size: 16px;
+}
+.destinatario {
+  text-align: center;
+  font-size: 25px;
+  font-weight: bold;
+  color: #1f497c; /* Azul profesional */
+  text-transform: uppercase;
+  margin-top: 5px;
+}
+.datos-comercio {
+  text-align: justify;
+  font-size: 15px;
+  line-height: 1;
+  margin-top: 10px;
+  margin-bottom: 40px;
+} 
+.resaltado2 {
+  font-weight: bold;
+  font-size: 15px;
+}  
+.bloque-autorizacion {
+  margin-top: 30px;
+  line-height: 1.5;
+}
+
+.autorizado {
+  font-size: 9px;
+  font-weight: bold;
+  font-family: 'Calibri', sans-serif;
+  color: #000000;
+}
 
     </style>
 </head>
@@ -175,9 +253,51 @@
 
 
 <div class="contenido">
+ 
+  <div class="bloque-centrado">
+    <div class="numero">No.: </div>
+    <div class="titulo">Dictamen de cumplimiento de Instalaciones</div>
+    <div class="subtitulo">como</div>
+    <div class="comercializador">Comercializador de Mezcal</div>
+  </div>
+ 
+  <p class="justificado">De acuerdo a lo establecido en los procedimientos internos de la Unidad de Inspección <span class="resaltado">No.UVNOM129</span> de la
+ Universidad Michoacana de San Nicolás de Hidalgo, con domicilio en Francisco J. Múgicas/n Col. Felicitas del
+ Rio, Morelia, Michoacán para la revisión de procesos de producción del producto Mezcal, su envasado y
+ comercialización; y confundamento en los artículos 56 Fracción I y 60 fracción I de la Ley de Infraestructura de la
+ Calidad que establece el funcionamiento de las Unidades de Inspección. Después de realizar la inspección de las
+ instalaciones en fecha del <u>10 de Junio del 2025</u> partiendo del acta circunstanciada o número de inspección:
+ <u>UMS-0725/2025</u> se otorga el Dictamen de Instalaciones</p>
+  <div class="bloque-centrado">
+ <div class="numero">A: </div>
+ </div>
+
+ <div class="bloque-centrado">
+  <div class="destinatario">GAO TAMLEI S.A DE C.V.</div>
+</div>
+
+<p class="datos-comercio">
+  <span class="resaltado2">Domicilio Fiscal:</span> Gaspar De Villadiego, 117, Nueva Valladolid, Morelia C.P. 58190<br>
+  <span class="resaltado2">Domicilio de la unidad de comercialización:</span> Gaspar De Villadiego, 117, Nueva Valladolid, Morelia, Michoacán. C.P. 58190<br>
+  <span class="resaltado2">Categorías de mezcal:</span> Mezcal, Mezcal Artesanal, Mezcal Ancestral<br>
+  <span class="resaltado2">Clases de mezcal que comercializa: </span>Blanco o Joven, Madurado en Vidrio, Reposado, Añejo, Abocado con, Destilado con<br>
+  <span class="resaltado2">Fecha de emisión del dictamen:</span>03 de Julio del 2025<br>
+  <span class="resaltado2">Fecha de vigencia del dictamen:</span> 03 de Julio del 2026
+</p>
 
 
+ <p class="justificado">El presente dictamen ampara exclusivamente la comercialización del producto mezcal que se realiza en las
+ instalaciones referidas en el presente documento. Dichas Instalaciones de comercialización cuentan con la
+ infraestructura y equipamiento requerido  para la comercialización de mezcal indicados en la NOM-070-SCFI-2016,
+ Bebidas alcohólicas-Mezcal- Especificaciones. 
+</p>
 
+
+<div class="bloque-autorizacion">
+  <div class="autorizado">AUTORIZÓ</div>
+  <div class="autorizado">Cadena Original</div>
+  <div class="autorizado">Sello Digital</div>
+</div>
 </div>
 
 
