b541f67708db8c954f2e27938f4fbc023654b68e
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-16 16:20:12 -0600

    revisiones

--- resources/views/certificados/add_revision.blade.php
+++ resources/views/certificados/add_revision.blade.php
@@ -743,6 +743,28 @@
                                         @elseif($pregunta->filtro == 'tipo_maguey')
                                             <td>
 
+                                                 @php
+                                                $solicitud = $datos->certificado->dictamen->inspeccione->solicitud;
+                                                $lote_granel = $solicitud->lote_granel;
+                                                $lote_envasado = $solicitud->lote_envasado;
+
+                                                 $ids = $solicitud->id_lote_envasado; // array de IDs
+                                                            $lotes_graneles = collect();
+
+                                                            foreach ($ids as $id) {
+                                                                $lote = App\Models\lotes_envasado::find($id);
+                                                                if ($lote) {
+                                                                    foreach ($lote->lotesGranel as $granel) {
+                                                                        if ($granel) {
+                                                                            $lotes_graneles->push($granel);
+                                                                        }
+                                                                    }
+                                                                }
+                                                            }
+
+                                                            
+                                            @endphp
+
                                                     @forelse ($datos->certificado->dictamen->inspeccione->solicitud->lote_granel->tiposRelacionados as $tipo)
                                                         {{ $tipo->nombre }} (<i>{{ $tipo->cientifico }}</i>),
                                                     @empty
