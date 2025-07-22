833a723a7755d074312f17d67374742cc85c5bcd
Jorge <jorgebravomendez6563@gmail.com>
2025-07-22 14:50:12 -0600

    avance del aparatado de olvide mi contraseña

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,7 +1,7 @@
 @php
-use Illuminate\Support\Facades\Route;
-$configData = Helper::appClasses();
-$customizerHidden = 'customizer-hide';
+  use Illuminate\Support\Facades\Route;
+  $configData = Helper::appClasses();
+  $customizerHidden = 'customizer-hide';
 @endphp
 
 @extends('layouts/blankLayout')
@@ -9,62 +9,77 @@ $customizerHidden = 'customizer-hide';
 @section('title', 'Forgot Password')
 
 @section('page-style')
-{{-- Page Css files --}}
-@vite('resources/assets/vendor/scss/pages/page-auth.scss')
+  {{-- Page Css files --}}
+  @vite('resources/assets/vendor/scss/pages/page-auth.scss')
 @endsection
 
 @section('content')
-<div class="authentication-wrapper authentication-cover">
-  <!-- Logo -->
-  <a href="{{url('/')}}" class="auth-cover-brand d-flex align-items-center gap-2">
-    <span class="app-brand-logo demo">@include('_partials.macros',["width"=>25,"withbg"=>'var(--bs-primary)'])</span>
-    <span class="app-brand-text demo text-heading fw-semibold">{{config('variables.templateName')}}</span>
-  </a>
-  <!-- /Logo -->
-  <div class="authentication-inner row m-0">
+  <div class="authentication-wrapper authentication-cover ">
+    <!-- Logo -->
+    <a href="{{ url('/') }}" class="auth-cover-brand d-flex align-items-center gap-2">
+    <span class="app-brand-logo demo img-logo"><img height="100px" src="{{ asset('assets/img/branding/logo_oc.png') }}"
+      alt=""></span>
+    </a>
+    <!-- /Logo -->
+    <div class="authentication-inner row m-0">
 
     <!-- /Left Section -->
-    <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-12 pb-2">
-      <img src="{{asset('assets/img/illustrations/auth-forgot-password-illustration-'.$configData['style'].'.png') }}" class="auth-cover-illustration w-100" alt="auth-illustration" data-app-light-img="illustrations/auth-forgot-password-illustration-light.png" data-app-dark-img="illustrations/auth-forgot-password-illustration-dark.png" />
-      <img src="{{asset('assets/img/illustrations/auth-cover-forgot-password-mask-'.$configData['style'].'.png') }}" class="authentication-image" alt="mask" data-app-light-img="illustrations/auth-cover-forgot-password-mask-light.png" data-app-dark-img="illustrations/auth-cover-forgot-password-mask-dark.png" />
+    <div class="d-none d-lg-flex col-lg-7 col-xl-8 align-items-center justify-content-center p-12 pb-2 min-vh-100">
+      <img style="inset-block-end: 0%" src="{{ asset('assets/img/fondo_oc_cidam3.jpg') }}"
+      class="authentication-image w-100 h-100 object-fit-cover" alt="mask" data-app-light-img="fondo_oc_cidam3.jpg"
+      data-app-dark-img="fondo_oc_cidam3.jpg" />
     </div>
+
     <!-- /Left Section -->
 
     <!-- Forgot Password -->
     <div class="d-flex col-12 col-lg-5 col-xl-4 align-items-center authentication-bg p-sm-12 p-6">
       <div class="w-px-400 mx-auto">
-        <h4 class="mb-1">¿Has olvidado tu contraseña?🔒</h4>
-        <p class="mb-5">Ingrese su correo electrónico y le enviaremos instrucciones para restablecer su contraseña</p>
+      <div class="text-center logo-up mb-4">
+        <a href="{{ url('/') }}" class="d-inline-block">
+        <img src="{{ asset('assets/img/branding/logo_oc.png') }}" alt="Logo CIDAM" height="90">
+        </a>
+      </div>
+      <h2 class="mb-3 text-heading">¿Olvidaste tu contraseña?</h2>
+      <p class="mb-4">Escribe tu correo electrónico y recibirás instrucciones para recuperarla de forma segura.</p>
 
-        @if (session('status'))
-        <div class="mb-1 text-success">
-          {{ session('status') }}
+
+      @if (session('status'))
+      <div class="alert alert-success d-flex align-items-center" role="alert">
+      <i class="ri-check-line me-2 fs-5"></i> <span>{{ session('status') }}</span>
+      </div>
+    @endif
+
+      <form id="formAuthentication" class="mb-5" action="{{ route('password.email') }}" method="POST">
+        @csrf
+        <div class="form-floating form-floating-outline mb-5">
+        <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email"
+          placeholder="ejemplo@.com" autofocus>
+        <label for="email">Email</label>
+        @error('email')
+      <div class="invalid-feedback d-block text-danger">
+        <i class="ri-error-warning-line me-1"></i> <strong>{{ $message }}</strong>
+      </div>
+      @enderror
+
         </div>
-        @endif
-        <form id="formAuthentication" class="mb-5" action="{{ route('password.email') }}" method="POST">
-           @csrf
-          <div class="form-floating form-floating-outline mb-5">
-            <input type="text" class="form-control @error('email') is-invalid @enderror" id="email" name="email" placeholder="john@example.com" autofocus>
-            <label for="email">Email</label>
-            @error('email')
-            <span class="invalid-feedback" role="alert">
-              <span class="fw-medium">{{ $message }}</span>
-            </span>
-            @enderror
-          </div>
-          <button type="submit" class="btn btn-primary d-grid w-100">Enviar enlace para restablecer contraseña</button>
-        </form>
-        <div class="text-center">
-          @if (Route::has('login'))
-            <a href="{{ route('login') }}" class="d-flex align-items-center justify-content-center">
-              <i class="ri-arrow-left-s-line scaleX-n1-rtl ri-20px me-1_5"></i>
-              Volver al inicio
-            </a>
-          @endif
-        </div>
+        <button type="submit" class="btn btn-primary w-100 shadow-sm py-2">
+        <i class="ri-lock-unlock-line me-2"></i> Enviar enlace
+        </button>
+
+      </form>
+      <div class="text-center mt-4">
+  @if (Route::has('login'))
+    <a href="{{ route('login') }}" class="btn btn-outline-secondary d-inline-flex align-items-center px-4 py-2 shadow-sm">
+      <i class="ri-arrow-left-s-line ri-lg me-2"></i>
+      Volver al inicio
+    </a>
+  @endif
+</div>
+
       </div>
     </div>
     <!-- /Forgot Password -->
+    </div>
   </div>
-</div>
 @endsection
