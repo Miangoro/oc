af8777491100df4726e948d7d8b7b8c46cd09350
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-23 10:43:02 -0600

    cambios en revision

--- resources/views/certificados/add_revision_consejo.blade.php
+++ resources/views/certificados/add_revision_consejo.blade.php
@@ -114,7 +114,7 @@
                     <div>
                         <p class="text-muted mb-1">Revisor</p>
                         <h5 class="fw-semibold mb-0"> <div class="avatar flex-shrink-0 me-4">
-                                            <img src="{{ asset('storage/' . $datos->user->foto) }}"
+                                            <img src="{{ asset('storage/' . $datos->user->profile_photo_path) }}"
                                                 alt="Foto de {{ $datos->user->name ?? 'N/A' }}" class="rounded-3" width="50">
                                         </div> {{ $datos->user->name ?? 'N/A' }}</h5>
                     </div>
