13e3c60ce9b15c0ce6f4df573351ef47d065b68b
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-24 10:50:50 -0600

    subir

--- app/Http/Controllers/getFuncionesController.php
+++ app/Http/Controllers/getFuncionesController.php
@@ -344,7 +344,7 @@ class getFuncionesController extends Controller
                 $urls_certificados->push($url);
             }
         }
-        if(empty($certificados)){
+        if(!$urls_certificados){
             $url = Documentacion_url::where('id_relacion', $idLote)
                     ->where('id_documento', 59)
                     ->value('url');
@@ -369,7 +369,7 @@ class getFuncionesController extends Controller
             }
         }
 
-          if(empty($certificados)){
+         if(!$fqs){
             $documentos2 = Documentacion_url::where('id_relacion', $idLote)
                 ->whereIn('id_documento', [58, 134])
                 ->get(['url', 'nombre_documento']);
