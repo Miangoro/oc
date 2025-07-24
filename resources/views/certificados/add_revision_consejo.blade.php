c9069495067a7c484dee2d2ff4e7d11ec6ee9f9e
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-07-23 18:08:02 -0600

    cambios generales

--- resources/views/certificados/add_revision_consejo.blade.php
+++ resources/views/certificados/add_revision_consejo.blade.php
@@ -806,6 +806,9 @@
 
                                                    
                                                     $empresa = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->certificadoGranel->dictamen->inspeccione->solicitud->empresa ?? null;
+                                                    if(!$empresa){
+                                                     $empresa = $datos->certificado->dictamen->inspeccione->solicitud->empresa ?? null;
+                                                    }
                                                     $numeroCliente = $empresa->empresaNumClientes->firstWhere('numero_cliente', '!=', null)->numero_cliente ?? null;
                                                     $url = \App\Models\documentacion_url::where('id_empresa', $empresa->id_empresa)
                                                     ->where('id_documento', 83)
@@ -828,6 +831,9 @@
                                                  @php
                                                      
                                                     $empresa = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel->certificadoGranel->dictamen->inspeccione->solicitud->empresa ?? null;
+                                                    if(!$empresa){
+                                                     $empresa = $datos->certificado->dictamen->inspeccione->solicitud->empresa ?? null;
+                                                    }
                                                     $numeroCliente = $empresa->empresaNumClientes->firstWhere('numero_cliente', '!=', null)->numero_cliente ?? null;
                                                     $url = \App\Models\documentacion_url::where('id_empresa', $empresa->id_empresa)
                                                     ->where('id_documento', 82)
