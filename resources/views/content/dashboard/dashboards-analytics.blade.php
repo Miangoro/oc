c7b0dddc37a73c3a58b1ec33d1cc5b660830cff8
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-08-04 12:51:36 -0600

    Cambios en el dasboard

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -544,7 +544,7 @@
                                             <tr>
                                                 <td class="ps-0 pe-12 py-4">
                                                     <span class="text-heading">
-                                                        {{ ucfirst(\Carbon\Carbon::parse($item->mes . '-01')->locale('es')->isoFormat('MMMM YYYY')) }}
+                                                        
                                                     </span>
                                                 </td>
                                                 <td class="text-end py-4">
