cea3beb0f6e0657268ec47d64fdd1f4ad40840c8
Ramses-Rojas <ches.rojas.torres@gmail.com>
2025-08-14 17:08:18 -0600

    etiquetas

--- app/Http/Controllers/certificados/Certificado_InstalacionesController.php
+++ app/Http/Controllers/certificados/Certificado_InstalacionesController.php
@@ -1080,7 +1080,7 @@ public function storeRevisor(Request $request)
             'id_sustituye' => $nombre_id_sustituye,
             //
             'razon_social' => $empresa->razon_social ?? 'No encontrado',
-            'cp' => $empresa->cp ?? ' ',
+            'cp' => $empresa->cp ?? 'x',
             'maestro_mezcalero' => $datos->maestro_mezcalero ?? '------------------------------',
             'numero_cliente' => $numero_cliente,
             'representante_legal' => $empresa->representante ?? 'No encontrado',
