344d50afb99041c046df5db647ce720ffdcf2f72
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-09-04 12:39:58 -0600

    Usuarios inactivos no pueden login

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,9 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use App\Http\Middleware\LocaleMiddleware;
+use Spatie\Permission\Middleware\PermissionMiddleware as MiddlewarePermissionMiddleware;
+use Spatie\Permission\Middleware\RoleMiddleware as MiddlewareRoleMiddleware;
+use Spatie\Permission\Middleware\RoleOrPermissionMiddleware as MiddlewareRoleOrPermissionMiddleware;
 use Spatie\Permission\Middlewares\RoleMiddleware;
 use Spatie\Permission\Middlewares\PermissionMiddleware;
 use Spatie\Permission\Middlewares\RoleOrPermissionMiddleware;
@@ -17,9 +20,9 @@ return Application::configure(basePath: dirname(__DIR__))
   ->withMiddleware(function (Middleware $middleware) {
     $middleware->web(LocaleMiddleware::class);
     $middleware->alias([
-            'role' => RoleMiddleware::class,
-            'permission' => PermissionMiddleware::class,
-            'role_or_permission' => RoleOrPermissionMiddleware::class,
+            'role' => MiddlewareRoleMiddleware::class,
+            'permission' => MiddlewarePermissionMiddleware::class,
+            'role_or_permission' => MiddlewareRoleOrPermissionMiddleware::class,
         ]);
   })
   ->withExceptions(function (Exceptions $exceptions) {
