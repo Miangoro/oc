f4b8f97e8c0abbc3a61d503569a0f1411dc1a22f
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-09-05 09:25:43 -0600

    exportacion

--- resources/views/pdfs/certificado_exportacion_ed13.blade.php
+++ resources/views/pdfs/certificado_exportacion_ed13.blade.php
@@ -370,7 +370,7 @@
 
         // old_hologramas
         if (!empty($oldHologramas[$clave])) {
-            $lineaConSaltos = str_replace(',', '<br>', $oldHologramas[$clave]);
+            $lineaConSaltos = str_replace(',', '<br>dfsdfsdf', $oldHologramas[$clave]);
             $contenido .= ($contenido ? '<br>' : '') . $lineaConSaltos;
         }
 
