5c25589e5c4048f62814fc8f5a1d264ba5520182
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-09-23 16:14:43 -0600

    revision cert nacional

--- app/Http/Controllers/revision/RevisionPersonalController.php
+++ app/Http/Controllers/revision/RevisionPersonalController.php
@@ -418,15 +418,21 @@ class RevisionPersonalController extends Controller
     public function add_revision($id_revision)
     {
         $datos = Revisor::with('certificadoNormal', 'certificadoGranel', 'certificadoExportacion')->where("id_revision", $id_revision)->first();
-         $preguntasQuery = preguntas_revision::where('tipo_revisor', 1)->where('tipo_certificado', $datos->tipo_certificado)->where('orden', $datos->numero_revision == 1 ? 0 : 1);
+     $preguntasQuery = preguntas_revision::where('tipo_revisor', 1)
+    ->where('tipo_certificado', $datos->tipo_certificado)
+    ->where('orden', ($datos->numero_revision == 1 ? 0 : 1)); // paréntesis
 
-        if ($datos->certificado->certificadoReexpedido()) {
-            $preguntasQuery->whereBetween('id_pregunta', [854, 860]);
-        }else{
-             $preguntasQuery->where('id_pregunta', '<',851)->where('id_pregunta', '>',868);
-        }
+if ($datos->certificado->certificadoReexpedido()) {
+    $preguntasQuery->whereBetween('id_pregunta', [854, 860]);
+} else {
+    $preguntasQuery->where(function ($q) {
+        $q->where('id_pregunta', '<', 851)
+          ->orWhere('id_pregunta', '>', 868);
+    });
+}
 
-        $preguntas = $preguntasQuery->get();
+$preguntas = $preguntasQuery->get();
+
        
      
 
