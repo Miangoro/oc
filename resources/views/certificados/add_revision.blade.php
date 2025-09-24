a389fbe77d1efc0bd9c255dfa13f7815acf5b31e
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-09-24 10:17:40 -0600

    holograma 052

--- resources/views/certificados/add_revision.blade.php
+++ resources/views/certificados/add_revision.blade.php
@@ -1100,23 +1100,7 @@ $loteGranel = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel
                                                                  $numero_cliente = $solic?->empresa?->empresaNumClientes
                                                             ->filter(fn($cliente) => !empty($cliente->numero_cliente))
                                                             ->first()?->numero_cliente ?? 'Sin asignar';
-                                                            foreach ($hologramasData as $rango) {
-                                                                
-                                                            
-
-                                                           
-                                                                $folioInicial = str_pad($rango['inicio'], 7, '0', STR_PAD_LEFT);
-                                                                $folioFinal = str_pad($rango['final'], 7, '0', STR_PAD_LEFT);
-
-                                                                $linkInicio = '<a target="_blank" href="/holograma/' . $numero_cliente . '-' .  $tipoHolograma . $folioMarca . $folioInicial . '">' .
-                                                                            $numero_cliente . '-' .  $tipoHolograma . $folioMarca . $folioInicial . '</a>';
-
-                                                                $linkFinal = '<a target="_blank" href="/holograma/' . $numero_cliente . '-' .  $tipoHolograma . $folioMarca . $folioFinal . '">' .
-                                                                            $numero_cliente . '-' .  $tipoHolograma . $folioMarca . $folioFinal . '</a>';
-
-                                                                $rangoFolios[] = $linkInicio . ' a ' . $linkFinal;
-                                                            
-                                                        }
+                                                   
                                                     @endphp
 
                                                     @foreach ($rangoFolios as $rango)
