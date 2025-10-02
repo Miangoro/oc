ef9d41a4217e6cf79a20e4a6b190c4e540c3e40b
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-10-02 15:40:03 -0600

    Cambios en rev

--- resources/views/certificados/add_revision_consejo.blade.php
+++ resources/views/certificados/add_revision_consejo.blade.php
@@ -1289,11 +1289,13 @@ $loteGranel = $datos->certificado->dictamen->inspeccione->solicitud->lote_granel
                                                             class="ri-file-pdf-2-fill text-danger ri-40px cursor-pointer"></i>
                                                     </a>
                                                 @else
-                                                    <a target="_blank"
-                                                        href="{{ $cliente_folder ? '../files/' . $cliente_folder . '/' . $datos->certificado->dictamen->inspeccione->solicitud->lote_envasado->etiquetas?->url_etiqueta->url : 'NA' }}">
-                                                        <i
-                                                            class="ri-file-pdf-2-fill text-danger ri-40px cursor-pointer"></i>
-                                                    </a>
+                                                   <a target="_blank"
+                                                        href="{{ $cliente_folder 
+                                                                ? '../files/' . $cliente_folder . '/' . ($datos->certificado?->dictamen?->inspeccione?->solicitud?->lote_envasado?->etiquetas?->url_etiqueta?->url ?? 'NA') 
+                                                                : 'NA' }}">
+                                                        <i class="ri-file-pdf-2-fill text-danger ri-40px cursor-pointer"></i>
+                                                        </a>
+
                                                 @endif
 
 
