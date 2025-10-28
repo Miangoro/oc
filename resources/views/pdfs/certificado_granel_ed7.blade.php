bd3f92fc321574c1a2bac23b837a6a2cbffac101
miguel.gmz.itsh@gmail.com <miguel.gmz.itsh@gmail.com>
2025-10-28 11:00:23 -0600

    preliminar en cert granel

--- resources/views/pdfs/certificado_granel_ed7.blade.php
+++ resources/views/pdfs/certificado_granel_ed7.blade.php
@@ -39,7 +39,7 @@
             opacity: 0.5;
             /* Opacidad predeterminada */
             letter-spacing: 3px;
-            font-size: 150px;
+            font-size: 110px;
             white-space: nowrap;
             z-index:-1;
         }
